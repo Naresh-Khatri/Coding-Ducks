281802f380c20e622204b95442ae45a578080501
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-15 16:51:12 +0530

    naise changes in mainlayout

--- layout/MainLayout.js
+++ layout/MainLayout.js
@@ -5,61 +5,69 @@ import {
   Flex,
   HStack,
   IconButton,
+  LightMode,
   Spacer,
   Text,
   useColorMode,
 } from "@chakra-ui/react";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { faLightbulb, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ProfileInfo from "../components/ProfileInfo";
 import Sidebar from "../components/Sidebar";
 
 function MainLayout({ children }) {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Flex h="80vh" flexDir={"column"}>
+    <Flex direction={"column"} h={"100vh"}>
       <Flex
         as="nav"
         alignItems={"center"}
-        h={70}
-        px={2}
-        bg="brand.900"
+        bg="purple.600"
         color={"white"}
         w={"100vw"}
+        px={2}
+        h={"70px"}
       >
-        <HStack alignItems={"center"}>
+        <HStack alignItems={"center"} h={"70px"}>
           <IconButton bg={"transparent"} />
-          <Text>Coding Ducks</Text>
+          <Text fontSize="20px" fontWeight={"extrabold"} noOfLines={1}>
+            Coding karlo frenzzzz
+          </Text>
         </HStack>
         <Spacer />
         <Box>
-          <Text> 00:10:43 </Text>
+          <Text fontWeight={"extrabold"}> 00:10:43 </Text>
         </Box>
         <Spacer />
         <HStack>
-          <Text>97/100</Text>
+          <Text fontWeight={"extrabold"}>97/100</Text>
           <Flex flexDir={"column"}>
-            <Text>naresh khatri</Text>
-            <Text>19fh1a0546</Text>
+            <Text fontWeight={"extrabold"}>naresh khatri</Text>
+            <Text fontWeight={"extrabold"}>19fh1a0546</Text>
           </Flex>
           <Avatar
             name="Kola Tioluwani"
             src="https://bit.ly/tioluwani-kolawole"
           />
           <IconButton
-            bg={"transparent"}
+            bg={colorMode === "light" ? "grey.900" : "grey.300"}
+            _hover={{ bg: colorMode === "light" ? "grey.900" : "grey.300" }}
             icon={
-              <FontAwesomeIcon icon={colorMode === "light" ? faSun : faMoon} />
+              <FontAwesomeIcon
+                icon={colorMode === "light" ? faMoon : faLightbulb}
+              />
             }
             onClick={toggleColorMode}
           />
+          <ProfileInfo />
           <Button variant={"solid"} bg={"red.400"}>
             Logout
           </Button>
         </HStack>
       </Flex>
-      <Flex w={"100vw"} h={"80%"} justifyContent="space-between">
-        {children}
+      <Flex w={"100vw"} flexGrow={1} overflowY="hidden">
         <Sidebar />
+        {children}
       </Flex>
     </Flex>
   );
