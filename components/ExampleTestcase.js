f3d061e92dc29deb35234e5082b62e56da4525dc
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-17 16:51:12 +0530

    create component to show problem test cases

---
+++ components/ExampleTestcase.js
@@ -0,0 +1,78 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+function ExampleTestcase({ testCase }) {
+  const styles = {
+    minHeight: "100px",
+    minWidth: "400px",
+    width: "-moz-fit-content",
+    width: "fit-content",
+    maxWidth: "800px",
+    color: 'white',
+    fontSize: "18px",
+    padding: "20px",
+    fontFamily: "Consolas,Courier New,monospace",
+    backdropFilter: "blur(4px) saturate(180%)",
+    WebkitBackdropFilter: "blur(16px) saturate(180%)",
+    backgroundColor: "#111928bf",
+    borderRadius: "12px",
+    border: "1px solid rgba(255,255,255,.125)",
+  };
+  return (
+    <Box style={styles}>
+      {testCase.input && (
+        <Box>
+          <svg height="20">
+            <circle
+              cx="6"
+              cy="6"
+              r="6"
+              fill="#FF5F56"
+              stroke="#E0443E"
+            ></circle>
+            <circle
+              cx="26"
+              cy="6"
+              r="6"
+              fill="#FFBD2E"
+              stroke="#DEA123"
+            ></circle>
+            <circle
+              cx="46"
+              cy="6"
+              r="6"
+              fill="#27C93F"
+              stroke="#1AAB29"
+            ></circle>
+          </svg>
+          <Text display="inline">Input:</Text>
+          <br />
+          <Text
+            as="span"
+            pl={5}
+            style={
+              testCase.inputFront.includes("<br>") ? { display: "block" } : {}
+            }
+            dangerouslySetInnerHTML={{ __html: testCase.inputFront }}
+          ></Text>
+        </Box>
+      )}
+      <Text display="inline">
+        Output:
+        <br />
+        <Text display="inline" pl={5}>
+          {testCase.output}
+        </Text>
+      </Text>
+      {testCase.explaination && (
+        <Text>
+          <br />
+          <Text>Explaination:</Text>
+          <Text pl={5}>{testCase.explaination}</Text>
+        </Text>
+      )}
+    </Box>
+  );
+}
+
+export default ExampleTestcase;
