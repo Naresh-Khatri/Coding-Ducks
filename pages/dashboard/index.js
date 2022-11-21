be195a38c770b7ca04d8f2735eeaf789c8febc47
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-21 16:51:12 +0530

    create admin dashboard page

---
+++ pages/dashboard/index.js
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import AdminLayout from "../../layout/AdminLayout";
+
+function index() {
+  return (
+    <>
+      <AdminLayout>
+        <Box>sdf</Box>
+      </AdminLayout>
+    </>
+  );
+}
+
+export default index;
