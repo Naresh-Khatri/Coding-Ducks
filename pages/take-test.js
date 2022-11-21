8cae398f17d8de11d8719e982fe0d63881a792b2
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-21 16:51:12 +0530

    wrap page inside <MainLayout> and define current problem state

--- pages/take-test.js
+++ pages/take-test.js
@@ -24,6 +24,7 @@ import OutputViewer from "../components/OutputViewer";
 import ProblemStatement from "../components/ProblemStatement";
 import ProblemSelector from "../components/ProblemSelector";
 import LeftProblemsList from "../components/LeftProblemsList";
+import MainLayout from "../layout/MainLayout";
 
 function TakeTest() {
   const [code, setCode] = useState("print('hi mom!')");
@@ -31,10 +32,12 @@ function TakeTest() {
   const [theme, setTheme] = useState("dracula");
   const [output, setOutput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [currentProblemId, setCurrentProblemId] = useState(8);
 
   const [problems, setProblems] = useState([]);
   useEffect(() => {
     fetch("https://coding_ducks.panipuri.tech/problems")
+    // fetch("http://localhost:4000/problems")
       .then((res) => res.json())
       .then((data) => {
         setProblems(data);
@@ -83,93 +86,99 @@ function TakeTest() {
   };
 
   return (
-    <Flex w={"100vw"} direction="row">
-      <Flex>
-        <LeftProblemsList problems={problems} />
-      </Flex>
-      {problems.length === 0 ? (
-        <Skeleton height="100vh" />
-      ) : (
-        <Flex flexGrow={1}>
-          <Split
-            className="split"
-            minSize={450}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <ProblemStatement problem={problems[8]} />
-            <Flex direction={"column"} width="100%" px={2}>
-              <ToolBar
-                isLoading={isLoading}
-                runCode={runCode}
-                lang={lang}
-                theme={theme}
-                setLang={setLang}
-                setTheme={setTheme}
-              />
-              <CodeEditor
-                code={code}
-                setCode={setCode}
-                lang={lang}
-                theme={theme}
-                runCode={runCode}
-              />
-              <Flex flexGrow={1} width="100%" height={"100%"}>
-                {isLoading && (
-                  <Stack bg="gray.800" h="100%">
-                    <Skeleton
-                      height="20px"
-                      fadeDuration={1}
-                      isLoaded={!isLoading}
-                    />
-                    <Skeleton
-                      height="20px"
-                      fadeDuration={3}
-                      isLoaded={!isLoading}
-                    />
-                    <Skeleton
-                      height="20px"
-                      fadeDuration={5}
-                      isLoaded={!isLoading}
-                    />
-                  </Stack>
-                )}
-                {!isLoading && (
-                  <Box w={"100%"}>
-                    <OutputViewer output={output} theme={theme} />
-                  </Box>
-                )}
-              </Flex>
-            </Flex>
-          </Split>
+    <MainLayout>
+      <Flex w={"100vw"} direction="row">
+        <Flex>
+          <LeftProblemsList
+            problems={problems}
+            currentProblemId={currentProblemId}
+            setCurrentProblemId={setCurrentProblemId}
+          />
         </Flex>
-      )}
-      <AlertDialog
-        isOpen={isAlertOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onAlertClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              MASTERRRRRRRRRRRR
-            </AlertDialogHeader>
+        {problems.length === 0 ? (
+          <Skeleton height="100vh" />
+        ) : (
+          <Flex flexGrow={1}>
+            <Split
+              className="split"
+              minSize={450}
+              style={{ height: "100%", width: "100%" }}
+            >
+              <ProblemStatement problem={problems[currentProblemId]} />
+              <Flex direction={"column"} width="100%" px={2}>
+                <ToolBar
+                  isLoading={isLoading}
+                  runCode={runCode}
+                  lang={lang}
+                  theme={theme}
+                  setLang={setLang}
+                  setTheme={setTheme}
+                />
+                <CodeEditor
+                  code={code}
+                  setCode={setCode}
+                  lang={lang}
+                  theme={theme}
+                  runCode={runCode}
+                />
+                <Flex flexGrow={1} width="100%" height={"100%"}>
+                  {isLoading && (
+                    <Stack bg="gray.800" h="100%">
+                      <Skeleton
+                        height="20px"
+                        fadeDuration={1}
+                        isLoaded={!isLoading}
+                      />
+                      <Skeleton
+                        height="20px"
+                        fadeDuration={3}
+                        isLoaded={!isLoading}
+                      />
+                      <Skeleton
+                        height="20px"
+                        fadeDuration={5}
+                        isLoaded={!isLoading}
+                      />
+                    </Stack>
+                  )}
+                  {!isLoading && (
+                    <Box w={"100%"}>
+                      <OutputViewer output={output} theme={theme} />
+                    </Box>
+                  )}
+                </Flex>
+              </Flex>
+            </Split>
+          </Flex>
+        )}
+        <AlertDialog
+          isOpen={isAlertOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onAlertClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                MASTERRRRRRRRRRRR
+              </AlertDialogHeader>
 
-            <AlertDialogBody>
-              Are you sure? You cant undo this action afterwards.
-            </AlertDialogBody>
+              <AlertDialogBody>
+                Are you sure? You cant undo this action afterwards.
+              </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onAlertClose} mr={4}>
-                Cancel
-              </Button>
-              <Button colorScheme="red" onClick={onAlertClose}>
-                Delete
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </Flex>
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onAlertClose} mr={4}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={onAlertClose}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Flex>
+    </MainLayout>
   );
 }
 
