9e16c8c6611d769214da650debfba371be8da86f
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-23 16:51:12 +0530

    allow domains for next <Image> component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "ik.imagekit.io",
+      "res.cloudinary.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
