308a7742f59c57b4ae49ed5cb3edc693f24f47fc
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-24 16:51:12 +0530

    create _document.js to link fonts

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
