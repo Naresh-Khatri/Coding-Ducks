8d5152fd2ed8315b59627cd77b8037cefc453520
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-28 16:51:12 +0530

    create an admin page to add exams

--- pages/dashboard/add-exam.js
+++ pages/dashboard/add-exam.js
@@ -7,34 +7,68 @@ import {
   Heading,
   Flex,
   FormControl,
-  GridItem,
   FormLabel,
   Input,
-  Select,
-  SimpleGrid,
-  InputLeftAddon,
   InputGroup,
-  Textarea,
   FormHelperText,
-  InputRightElement,
   Text,
+  HStack,
 } from "@chakra-ui/react";
 
 import AdminLayout from "../../layout/AdminLayout";
 
 import { useToast } from "@chakra-ui/react";
 
+import "react-quill/dist/quill.snow.css";
+
 import dynamic from "next/dynamic";
+import Image from "next/image";
+import axios from "axios";
+import { useRouter } from "next/router";
 const QuillNoSSRWrapper = dynamic(import("react-quill"), {
   ssr: false,
   loading: () => <p>Loading ...</p>,
 });
-import "react-quill/dist/quill.snow.css";
 
 const AddExam = () => {
   const [show, setShow] = useState(false);
   const [desc, setDesc] = useState("");
-  const handleClick = () => setShow(!show);
+  const [title, setTitle] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [coverImg, setCoverImg] = useState("");
+
+  const toast = useToast();
+  const router = useRouter();
+
+  const submit = async () => {
+    try {
+      const payload = {
+        title,
+        description: desc,
+        startTime: new Date(startTime).toISOString(),
+        coverImg,
+      };
+      const res = await axios.post("http://localhost:3333/exams", payload);
+      toast({
+        title: "Exam created!",
+        status: "success",
+        description: "New exam has been created.",
+        duration: 9000,
+        isClosable: true,
+      });
+      router.push("/dashboard/exams");
+    } catch (error) {
+      toast({
+        title: "Please check the fields!",
+        description: "An error has occured.",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <AdminLayout>
@@ -49,14 +83,34 @@ const AddExam = () => {
             >
               Create an exam!
             </Heading>
-            <Flex>
+            <HStack my={3}>
               <FormControl mr="5%">
                 <FormLabel htmlFor="first-name" fontWeight={"normal"}>
                   Title
                 </FormLabel>
-                <Input id="first-name" placeholder="First name" />
+                <Input
+                  id="first-name"
+                  placeholder="Title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </FormControl>{" "}
+              <FormControl>
+                <FormLabel fontWeight={"normal"} mt="2%">
+                  Start time
+                </FormLabel>
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type={"datetime-local"}
+                    placeholder="Enter password"
+                    value={startTime}
+                    onChange={(e) => setStartTime(e.target.value)}
+                  />
+                </InputGroup>
               </FormControl>
-            </Flex>
+            </HStack>
+
             <FormControl mt="2%">
               <FormLabel htmlFor="email" fontWeight={"normal"}>
                 Description
@@ -67,25 +121,33 @@ const AddExam = () => {
                   value={desc}
                   onChange={setDesc}
                 />
-                ;
               </Box>
               <FormHelperText>Describe about your exam</FormHelperText>
             </FormControl>
 
-            <FormControl>
-              <FormLabel fontWeight={"normal"} mt="2%">
-                Start time
-              </FormLabel>
-              <InputGroup size="md">
+            <Flex>
+              <FormControl mr="5%">
+                <FormLabel htmlFor="cover-img" fontWeight={"normal"}>
+                  Cover photo (URL)
+                </FormLabel>
                 <Input
-                  pr="4.5rem"
-                  type={"datetime-local"}
-                  placeholder="Enter password"
+                  id="cover-img"
+                  placeholder="Image URL"
+                  value={coverImg}
+                  onChange={(e) => setCoverImg(e.target.value)}
                 />
-              </InputGroup>
-            </FormControl>
+              </FormControl>
+              {coverImg && (
+                <img
+                  src={coverImg}
+                  alt="cover image"
+                  height="200"
+                  width="auto"
+                />
+              )}
+            </Flex>
             <FormControl>
-              <Button bg="green.500" mt={3}>
+              <Button bg="green.500" mt={3} onClick={submit}>
                 <Text fontWeight={"extrabold"} fontSize={"2xl"}>
                   Create
                 </Text>
