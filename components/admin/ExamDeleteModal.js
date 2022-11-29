9311547d5df6db54c33b53036fcb939b8d721c5b
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-29 16:51:12 +0530

    create a modal to remove exam

---
+++ components/admin/ExamDeleteModal.js
@@ -0,0 +1,75 @@
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React from "react";
+import ExamCard from "../ExamCard";
+
+function ExamDeleteModal({ examData, isOpen, onClose, onDeleteSuccess }) {
+  const toast = useToast();
+  const deleteExam = async () => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:3333/exams/${examData.id}`
+      );
+      console.log(res.data);
+      toast({
+        title: "Exam deleted!",
+        status: "success",
+        description: "Exam has been deleted.",
+        duration: 9000,
+        isClosable: true,
+      });
+      onDeleteSuccess();
+    } catch (err) {
+      console.log(err);
+      toast({
+        title: "Error",
+        description: "Something went wrong while Deteting!",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{examData.title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>Are you sure you wanna delete this exam?</Text>
+          <Flex justify={"center"} p={5} pointerEvents={"none"}>
+            <ExamCard examData={examData} />
+          </Flex>
+        </ModalBody>
+
+        <ModalFooter>
+          <HStack>
+            <Button variant={"outline"} onClick={onClose}>
+              Close
+            </Button>
+            <Button bg="red.500" onClick={deleteExam}>
+              Delete
+            </Button>
+          </HStack>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default ExamDeleteModal;
