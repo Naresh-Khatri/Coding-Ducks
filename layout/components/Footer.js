c08954944eddfe508b95bca40d69aafbcfbc1115
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-30 16:51:12 +0530

    create a simple footer component

---
+++ layout/components/Footer.js
@@ -0,0 +1,120 @@
+import {
+  Button,
+  ButtonGroup,
+  Container,
+  Divider,
+  IconButton,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import * as React from "react";
+// import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Footer = () => (
+  <Container as="footer" role="contentinfo" maxW={"7xl"}>
+    <Stack
+      spacing="8"
+      direction={{ base: "column", md: "row" }}
+      justify="space-between"
+      py={{ base: "12", md: "16" }}
+    >
+      <Stack spacing={{ base: "6", md: "8" }} align="start">
+        <Text
+          fontSize="3xl"
+          fontWeight="bold"
+          lineHeight={0}
+          color="purple.600"
+        >
+          Coding Ducks
+        </Text>
+        <Text color="muted" lineHeight={0}>
+          Learn to code remarkably fast.
+        </Text>
+      </Stack>
+      <Stack
+        direction={{ base: "column-reverse", md: "column", lg: "row" }}
+        spacing={{ base: "12", md: "8" }}
+      >
+        <Stack direction="row" spacing="8">
+          <Stack spacing="4" minW="36" flex="1">
+            <Text fontSize="sm" fontWeight="semibold" color="subtle">
+              Product
+            </Text>
+            <Stack spacing="3" shouldWrapChildren>
+              <Button variant="link">How it works</Button>
+              <Button variant="link">Pricing</Button>
+              <Button variant="link">Use Cases</Button>
+            </Stack>
+          </Stack>
+          <Stack spacing="4" minW="36" flex="1">
+            <Text fontSize="sm" fontWeight="semibold" color="subtle">
+              Legal
+            </Text>
+            <Stack spacing="3" shouldWrapChildren>
+              <Button variant="link">Privacy</Button>
+              <Button variant="link">Terms</Button>
+              <Button variant="link">License</Button>
+            </Stack>
+          </Stack>
+        </Stack>
+        <Stack spacing="4">
+          <Text fontSize="sm" fontWeight="semibold" color="subtle">
+            Stay up to date
+          </Text>
+          <Stack
+            spacing="4"
+            direction={{ base: "column", sm: "row" }}
+            maxW={{ lg: "360px" }}
+          >
+            <Input placeholder="Enter your email" type="email" required />
+            <Button variant="primary" type="submit" flexShrink={0}>
+              Subscribe
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
+    <Divider />
+    <Stack
+      pt="8"
+      pb="12"
+      justify="space-between"
+      direction={{ base: "column-reverse", md: "row" }}
+      align="center"
+    >
+      <Text fontSize="sm" color="subtle">
+        &copy; {new Date().getFullYear()} Coding Ducks, Inc. All rights
+        reserved.
+      </Text>
+      <ButtonGroup variant="ghost">
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="LinkedIn"
+          icon={<FontAwesomeIcon height={"1.2rem"} icon={faLinkedin} />}
+        />
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="GitHub"
+          icon={<FontAwesomeIcon height={"1.2rem"} icon={faGithub} />}
+        />
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="Twitter"
+          icon={<FontAwesomeIcon height={"1.2rem"} icon={faTwitter} />}
+        />
+      </ButtonGroup>
+    </Stack>
+  </Container>
+);
+
+export default Footer;
