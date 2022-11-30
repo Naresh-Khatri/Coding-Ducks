163f8b0e7b689ecc8051a5e969556e6392e39b67
naresh-khatri <naresh.khatri2345@gmail.com>
2022-11-30 16:51:12 +0530

    create a basic axios instance for adding auth header in every req

---
+++ utils/axios.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  //   baseURL: "https://ducks.panipuri.tech",
+  baseURL: "http://localhost:3333",
+  transformRequest: [
+    (data, headers) => {
+      // modify data here
+      if (typeof window !== "undefined") {
+        const cookieText = document.cookie;
+        const token = cookieText.split("token=")[1];
+        if (token) {
+          headers["Authorization"] = `Bearer ${token}`;
+        }
+        // console.log(headers['Authorization']);
+      }
+      return data;
+    },
+    ...axios.defaults.transformRequest,
+  ],
+  transformResponse: [
+    ...axios.defaults.transformResponse,
+    (data) => {
+      // modify data here
+      // clear cookie and cache and redirect to login page
+      console.log(data)
+      if (data.code == 401 && typeof window !== "undefined")
+        window.location.href = "/login";
+      return data;
+    },
+  ],
+});
+
+export default axiosInstance;
