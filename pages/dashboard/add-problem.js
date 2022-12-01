efc43e4eb8d48abdd0d8bc7591a52cf614a65bf9
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-01 16:51:12 +0530

    create basic page for uploading questions

--- pages/dashboard/add-problem.js
+++ pages/dashboard/add-problem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Box,
   Button,
@@ -14,6 +14,18 @@ import {
   RadioGroup,
   Radio,
   Stack,
+  Switch,
+  Textarea,
+  IconButton,
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Container,
+  Select,
 } from "@chakra-ui/react";
 
 import AdminLayout from "../../layout/AdminLayout";
@@ -23,8 +35,9 @@ import { useToast } from "@chakra-ui/react";
 import "react-quill/dist/quill.snow.css";
 
 import dynamic from "next/dynamic";
-import axios from "axios";
 import { useRouter } from "next/router";
+import TestCaseRow from "../../components/admin/TestCaseRow";
+import axios from "../../utils/axios";
 const QuillNoSSRWrapper = dynamic(import("react-quill"), {
   ssr: false,
   loading: () => <p>Loading ...</p>,
@@ -34,12 +47,31 @@ const AddExam = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [diffLevel, setDiffLevel] = useState("easy");
+  const [examsList, setExamsList] = useState([]);
+  const [testCases, setTestCases] = useState([
+    { id: 0, input: "", output: "", explaination: "", isPublic: false },
+  ]);
+
+  const selectedExam = useRef(null);
+  // const [tags, setTags] = useState([]);
 
   const router = useRouter();
 
   const toast = useToast();
   const submit = async () => {
+    const payload = {
+      title,
+      description: desc,
+      difficulty: diffLevel,
+      tags: "aksdfl",
+      testCases,
+      examId: +selectedExam.current.value,
+    };
+    // console.log(payload)
+    // return
     try {
+      const res = await axios.post("/problems", payload);
+      console.log(res);
       toast({
         title: "Exam created!",
         status: "success",
@@ -47,7 +79,7 @@ const AddExam = () => {
         duration: 9000,
         isClosable: true,
       });
-      router.push("/dashboard/exams");
+      // router.push("/dashboard/problems");
     } catch (error) {
       toast({
         title: "Please check the fields!",
@@ -60,11 +92,27 @@ const AddExam = () => {
     }
   };
 
+  const fetchExams = async () => {
+    const res = await axios.get("/exams");
+    setExamsList(res.data);
+    // console.log(res);
+  };
+
+  useEffect(() => {
+    fetchExams();
+  }, []);
+
   return (
     <>
       <AdminLayout>
-        <Flex direction={"column"} p={10} w={"100%"} alignItems="center">
-          <Box w={"900px"}>
+        <Flex
+          direction={"column"}
+          p={10}
+          w={"100%"}
+          alignItems="center"
+          overflowY="scroll"
+        >
+          <Container maxW={"8xl"}>
             <Heading
               w="100%"
               textAlign={"center"}
@@ -72,7 +120,7 @@ const AddExam = () => {
               fontSize={"4xl"}
               mb={20}
             >
-              Create an exam!
+              Create a problem!
             </Heading>
             <HStack my={3}>
               <FormControl mr="5%">
@@ -108,26 +156,68 @@ const AddExam = () => {
               </Box>
               <FormHelperText>Describe about your exam</FormHelperText>
             </FormControl>
-
+            <FormControl mt="2%">
+              <FormLabel htmlFor="email" fontWeight={"normal"}>
+                Select Exam
+              </FormLabel>
+              <Select ref={selectedExam}>
+                {examsList.map((exam) => (
+                  <option key={exam.id} value={exam.id}>
+                    {`${exam.title} - /exmas/${exam.slug}`}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
             <Flex mt={10}>
-              <FormControl>
-                <FormLabel htmlFor="cover-img" fontWeight={"normal"}>
-                  TestCases
-                </FormLabel>
-                <Input
-                  id="cover-img"
-                  placeholder="Image URL"
-                  // value={coverImg}
-                  // onChange={(e) => setCoverImg(e.target.value)}
-                />
-              </FormControl>
+              <TableContainer w={"100%"}>
+                <Text>Test Cases: {testCases.length}</Text>
+                <Table variant="simple">
+                  {/* <TableCaption>Exams</TableCaption> */}
+                  <Thead>
+                    <Tr>
+                      <Th>Id</Th>
+                      <Th>Input</Th>
+                      <Th>Output</Th>
+                      <Th>explaination</Th>
+                      <Th>Private</Th>
+                      <Th>Remove</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {testCases.map((testCase, index) => (
+                      <TestCaseRow
+                        testCases={testCases}
+                        key={index}
+                        index={index}
+                        setTestCases={setTestCases}
+                      />
+                    ))}
+                  </Tbody>
+                </Table>
+                <Button
+                  onClick={() => {
+                    setTestCases((p) => [
+                      ...p,
+                      {
+                        id: p.length + 1,
+                        input: "",
+                        output: "",
+                        explaination: "",
+                        isPublic: false,
+                      },
+                    ]);
+                  }}
+                >
+                  Add a test case
+                </Button>
+              </TableContainer>
             </Flex>
             <FormControl>
               <Button bg="green.500" mt={3} onClick={submit}>
                 Create
               </Button>
             </FormControl>
-          </Box>
+          </Container>
         </Flex>
       </AdminLayout>
     </>
