a92a4b9e899d4e4ff37d25503ffef0691ba4f888
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-10 16:51:12 +0530

    create context to store and manage user submissions

---
+++ contexts/submissionsContext.js
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "../utils/axios";
+
+export const submissionsContext = createContext({
+  marks: 0,
+  totalMarks: 100,
+  submisisons: [{}],
+  refreshSubmissions: null,
+});
+
+export const SubmissionProvider = ({ children }) => {
+  const [submissions, setSubmissions] = useState([]);
+  const [marks, setMarks] = useState(0);
+  const [totalMarks, setTotalMarks] = useState(0);
+
+  const formatSubmission = (data) => {
+    setTotalMarks(data.totalMarks);
+
+    // get best submissions
+    let bestSubs = [];
+    data.submissions.forEach((submission) => {
+      if (bestSubs.length === 0) bestSubs.push(submission);
+      if (bestSubs[bestSubs.length - 1].problemId === submission.problemId) {
+        if (bestSubs[bestSubs.length - 1].marks < submission.marks) {
+          bestSubs[bestSubs.length - 1] = submission;
+        }
+      } else {
+        bestSubs.push(submission);
+      }
+    });
+    console.log(bestSubs);
+    setSubmissions(bestSubs);
+    let marksObtained = bestSubs.reduce((acc, curr) => acc + curr.marks, 0);
+    setMarks(marksObtained);
+  };
+  const refreshSubmissions = async (examId = 2) => {
+    try {
+      const res = await axios.get(`/exams/getProgress/${examId}`);
+      formatSubmission(res.data);
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+  useEffect(() => {
+    refreshSubmissions();
+  }, []);
+  return (
+    <submissionsContext.Provider
+      value={{
+        submissions,
+        setSubmissions,
+        marks,
+        totalMarks,
+        refreshSubmissions,
+      }}
+    >
+      {children}
+    </submissionsContext.Provider>
+  );
+};
