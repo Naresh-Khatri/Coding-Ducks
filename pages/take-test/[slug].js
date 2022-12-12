c055aa57230cc47dab099105db4ae77bb8f9a232
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-12 18:20:19 +0530

    stuff

--- pages/take-test/[slug].js
+++ pages/take-test/[slug].js
@@ -20,8 +20,10 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 import Split from "react-split";
+import axios from "../../utils/axios";
 
 import ToolBar from "../../components/ToolBar";
 import CodeEditor from "../../components/CodeEditor";
@@ -29,13 +31,11 @@ import OutputViewer from "../../components/OutputViewer";
 import ProblemStatement from "../../components/ProblemStatement";
 import LeftProblemsList from "../../components/LeftProblemsList";
 import MainLayout from "../../layout/MainLayout";
-import { userContext } from "../../contexts/userContext";
-import axios from "../../utils/axios";
-import { useRouter } from "next/router";
+import SubmissionModal from "../../components/modals/Submission";
+
 import { submissionsContext } from "../../contexts/submissionsContext";
 
 function TakeTest() {
-  const { user } = useContext(userContext);
   const { refreshSubmissions } = useContext(submissionsContext);
 
   const [code, setCode] = useState("");
@@ -43,7 +43,8 @@ function TakeTest() {
   const [theme, setTheme] = useState("dracula");
   const [output, setOutput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [currentProblemId, setCurrentProblemId] = useState(2);
+  const [currentProblemIdx, setCurrentProblemIdx] = useState(2);
+  const [lastSubmissionPassed, setLastSubmissionPassed] = useState(false);
 
   const [problems, setProblems] = useState([]);
 
@@ -54,12 +55,12 @@ function TakeTest() {
 
   useEffect(() => {
     setCode(
-      localStorage.getItem(`code ${examData?.id} ${currentProblemId}`) || ""
+      localStorage.getItem(`code ${examData?.id} ${currentProblemIdx}`) || ""
     );
-  }, [currentProblemId]);
+  }, [currentProblemIdx]);
 
   useEffect(() => {
-    localStorage.setItem(`code ${examData?.id} ${currentProblemId}`, code);
+    localStorage.setItem(`code ${examData?.id} ${currentProblemIdx}`, code);
   }, [code]);
 
   const fetchData = useCallback(async () => {
@@ -68,9 +69,9 @@ function TakeTest() {
 
       let res = await axios.get(`/exams/slug/${router.query.slug}`);
       setExamData(res.data);
-      console.log(examData?.id);
+      refreshSubmissions(res.data.id);
       res = await axios.get("/problems/examProblems/" + res.data.id);
-      setProblems(res.data);
+      setProblems(res.data.sort((a, b) => a.order - b.order));
     } catch (error) {
       console.log("error", error);
       router.push("/home");
@@ -79,7 +80,10 @@ function TakeTest() {
 
   useEffect(() => {
     fetchData();
-    setTimeout(() => setCurrentProblemId(1), 1000);
+
+    setTimeout(() => {
+      setCurrentProblemIdx(1);
+    }, 1000);
   }, [router.query.slug, fetchData]);
 
   const {
@@ -88,25 +92,29 @@ function TakeTest() {
     onClose: onAlertClose,
   } = useDisclosure();
 
+  const {
+    isOpen: isSubmissionModalOpen,
+    onOpen: onSubmissionModalOpen,
+    onClose: onSubmissionModalClose,
+  } = useDisclosure();
   const cancelRef = useRef();
 
   const toast = useToast();
   const runCode = async (submit = false) => {
     setIsLoading(true);
-    console.log(examData);
     const payload = {
       code,
       lang,
       submit,
-      problemId: problems[currentProblemId].id - 1,
+      problemId: problems[currentProblemIdx - 1].id,
       examId: examData.id,
     };
     try {
       const res = await axios.post("/runCode", payload);
-      // console.log(res.data);
       if (submit) {
-        // console.log(refreshSubmissions);
-        refreshSubmissions();
+        refreshSubmissions(examData?.id);
+        setLastSubmissionPassed(res.data.isCorrect);
+        onSubmissionModalOpen();
       }
       setOutput(res.data);
       setIsLoading(false);
@@ -131,8 +139,8 @@ function TakeTest() {
         <Flex>
           <LeftProblemsList
             problems={problems}
-            currentProblemId={currentProblemId}
-            setCurrentProblemId={setCurrentProblemId}
+            currentProblemIdx={currentProblemIdx}
+            setCurrentProblemIdx={setCurrentProblemIdx}
           />
         </Flex>
         {problems.length === 0 ? (
@@ -144,7 +152,7 @@ function TakeTest() {
               minSize={450}
               style={{ height: "100%", width: "100%" }}
             >
-              <ProblemStatement problem={problems[currentProblemId - 1]} />
+              <ProblemStatement problem={problems[currentProblemIdx - 1]} />
               <Flex direction={"column"} width="100%" px={2}>
                 <ToolBar
                   isLoading={isLoading}
@@ -222,6 +230,15 @@ function TakeTest() {
             </AlertDialogContent>
           </AlertDialogOverlay>
         </AlertDialog>
+        <SubmissionModal
+          onClose={() => {
+            setLastSubmissionPassed(false);
+            onSubmissionModalClose();
+          }}
+          onOpen={onSubmissionModalOpen}
+          isOpen={isSubmissionModalOpen}
+          passed={lastSubmissionPassed}
+        />
       </Flex>
     </MainLayout>
   );
