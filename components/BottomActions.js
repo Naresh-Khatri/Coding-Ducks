68aa26d70cbe68e6094da5e8d54656fec76d3f30
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-15 18:43:36 +0530

    create new component to run and submit code

---
+++ components/BottomActions.js
@@ -0,0 +1,43 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, HStack, Spacer } from "@chakra-ui/react";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+function BottomActions({ setShowConsole, showConsole, isLoading, runCode }) {
+  return (
+    <Flex w="100%" justify={"space-between"} pb={5}>
+      <Button
+        onClick={() => setShowConsole((p) => !p)}
+        rightIcon={showConsole ? <ChevronDownIcon /> : <ChevronUpIcon />}
+      >
+        Console
+      </Button>
+      <HStack>
+        <Button
+          loadingText="Running..."
+          isLoading={isLoading}
+          bg="purple.600"
+          leftIcon={<FontAwesomeIcon icon={faReact} />}
+          color="white"
+          onClick={() => runCode(false)}
+        >
+          Run
+        </Button>
+        <Button
+          loadingText="Running..."
+          isLoading={isLoading}
+          bg="green.400"
+          leftIcon={<FontAwesomeIcon icon={faPlay} />}
+          color="white"
+          onClick={() => runCode(true)}
+        >
+          Submit
+        </Button>
+      </HStack>
+    </Flex>
+  );
+}
+
+export default BottomActions;
