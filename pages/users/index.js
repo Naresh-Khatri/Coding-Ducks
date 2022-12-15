67acc43a5bbb566d5a79b756b0239fe5e686e7b3
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-15 18:49:07 +0530

    create new page to view all users

---
+++ pages/users/index.js
@@ -0,0 +1,83 @@
+import { Box, Container, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import NormalLayout from "../../layout/NormalLayout";
+import axios from "../../utils/axios";
+
+function UsersPage({ users }) {
+  return (
+    <NormalLayout>
+      <Container mt={70} maxW={"6xl"} minH={"100vh"}>
+        <Text fontSize="4xl" fontWeight="bold" mb={10}>
+          Users
+        </Text>
+        <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={10}>
+          {users.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </SimpleGrid>
+      </Container>
+    </NormalLayout>
+  );
+}
+
+const UserCard = ({ user }) => {
+  return (
+    <Link href={`/users/${user.username}`}>
+      <Box
+        w={"full"}
+        h={200}
+        position="relative"
+        bg={"gray.700"}
+        backgroundColor="#111928bf"
+        backdropFilter="blur(4px) saturate(180%)"
+        borderRadius="12px"
+        border="1px solid rgba(255,255,255,.125)"
+        _hover={{ backgroundColor: "#111528" }}
+      >
+        <Box
+          position="absolute"
+          top={-10}
+          left={"25%"}
+          _hover={{ top: -55 }}
+          transition="all .1s ease-in-out"
+        >
+          <Image
+            src={user.photoURL}
+            alt="Picture of the author"
+            width={130}
+            height={130}
+            style={{ borderRadius: "50%" }}
+          />
+        </Box>
+        <Flex mt={100} align="center" direction={"column"}>
+          <Text fontWeight={"extrabold"} fontSize={"xl"}>
+            {user.fullname}
+          </Text>
+          <Text color={"gray"}>@{user.username}</Text>
+        </Flex>
+      </Box>
+    </Link>
+  );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const { data } = await axios.get("/users");
+    return {
+      props: {
+        users: data,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {
+        users: [],
+      },
+    };
+  }
+};
+
+export default UsersPage;
