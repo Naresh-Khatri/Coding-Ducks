06ce3ebcde63c581a05a0103f03a4588fd13a198
naresh-khatri <naresh.khatri2345@gmail.com>
2022-12-15 18:51:14 +0530

    show user stats

--- pages/users/[username].js
+++ pages/users/[username].js
@@ -1,6 +1,7 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import UserInfo from "../../components/UserInfo";
+import UserStats from "../../components/UserStats";
 
 import NormalLayout from "../../layout/NormalLayout";
 import axios from "../../utils/axios";
@@ -8,18 +9,22 @@ import axios from "../../utils/axios";
 function UsersPage({ username }) {
   // console.log(user);
   const [user, setUser] = useState();
+  const [progress, setProgress] = useState();
   useEffect(() => {
     async function fetchUser() {
       const user = await getUserByUsername(username);
       setUser(user);
+      const progress = await getUserProgress(user.id);
+      setProgress(progress);
     }
     fetchUser();
-  });
+  }, []);
   return (
     <NormalLayout>
-      <Container maxW={"8xl"}>
-        <Flex justify={"center"} align="center">
-          {user && <UserInfo viewingUser={user} />}
+      <Container maxW={"8xl"} minH={"100vh"}>
+        <Flex align="center" justify={"center"} w={"100%"} h={"100%"}>
+          <Flex>{user && <UserInfo viewingUser={user} />}</Flex>
+          <Flex flexGrow={1}>{progress && <UserStats progress={progress} />}</Flex>
         </Flex>
       </Container>
     </NormalLayout>
@@ -39,6 +44,10 @@ export const getServerSideProps = async (ctx) => {
 
 const getUserByUsername = async (username) => {
   const user = await axios.get("/users/username/" + username);
+  return user.data;
+};
+const getUserProgress = async (userId) => {
+  const user = await axios.get("/users/progress/" + userId);
   return user.data;
 };
 
