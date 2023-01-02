fa579ef86acc89a19c7899b44122544f52f8ae81
naresh-khatri <naresh.khatri2345@gmail.com>
2023-01-02 16:51:12 +0530

    improve loading overlay component

--- components/LoadingOverlay.js
+++ components/LoadingOverlay.js
@@ -1,23 +1,49 @@
-import { Box, Center, Spinner, Text } from "@chakra-ui/react"
+import { Box, Center, Spinner, Text } from "@chakra-ui/react";
+import Router from "next/router";
+import { useContext, useEffect, useState } from "react";
+import { userContext } from "../contexts/userContext";
+function LoadingOverlay(children) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [timer, setTimer] = useState(null);
+  const {loading: userIsLoading} = useContext(userContext)
 
-function LoadingOverlay() {
-    return (
+  useEffect(()=>{
+    setIsLoading(userIsLoading)
+  }, [userIsLoading])
+  Router.events.on("routeChangeStart", () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(() => {
+        setIsLoading(true);
+      }, 0)
+    );
+  });
+  Router.events.on("routeChangeComplete", () => {
+    clearTimeout(timer);
+    setIsLoading(false);
+  });
+  return (
+    <>
+      {isLoading && (
         <Box
-            position={'absolute'}
-            zIndex='100'
-            h={'100vh'}
-            w='100vw'
-            top={0}
-            bottom={0}
-            style={{ background: '#33333377' }}>
-            <Center h={'100%'} flexDir='column' color={'white'}>
-                <Spinner h={100} w={100} thickness={10} />
-                <Text mt={5} fontSize={'xl'}>
-                    Waiting...
-                </Text>
-            </Center>
+          position={"absolute"}
+          zIndex="100"
+          h={"100vh"}
+          w="100vw"
+          top={0}
+          bottom={0}
+          style={{ background: "#33333377" }}
+        >
+          <Center h={"100%"} flexDir="column" color={"white"}>
+            <Spinner h={100} w={100} thickness={10} />
+            <Text mt={5} fontSize={"xl"}>
+              Waiting...
+            </Text>
+          </Center>
         </Box>
-    )
+      )}
+    </>
+  );
 }
 
-export default LoadingOverlay
+export default LoadingOverlay;
