316fa9d34e8b7016e66ddf7ec38d726dcae1723a
naresh-khatri <naresh.khatri2345@gmail.com>
2023-01-14 16:51:12 +0530

    create custom hooks to fetch exams and problems

---
+++ hooks/useExamsData.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../utils/axios";
+
+export const useExamsData = () => {
+    return useQuery(["exams"], () => axiosInstance.get("/exams"));
+};
+
+export const useExamData = (examSlug: string) => {
+    return useQuery(
+        ["exam", examSlug],
+        () => axiosInstance.get(`/exams/slug/${examSlug}`),
+    );
+};
+
+export const useExamProblemsData = ({ examId, enabled, }: {
+    examId: number; enabled: boolean;
+}) => {
+    console.log(examId, enabled);
+    return useQuery(
+        ["examProblems", examId],
+        () => axiosInstance.get(`/problems/examProblems/${examId}`),
+        { refetchOnMount: false, enabled: enabled }
+    );
+};
