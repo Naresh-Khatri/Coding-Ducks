13cdf4c73d03fa3ae4fbf2872f3469de7a55e6bd
naresh-khatri <naresh.khatri2345@gmail.com>
2023-01-16 16:51:12 +0530

    create custom hooks to fetch user and their progress

---
+++ hooks/useUsersData.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "../utils/axios"
+
+interface User {
+    id: number
+    fullname: string;
+    photoURL: string;
+    rank: number;
+    registeredAt: string;
+    totalMarks: number;
+    username: string
+}
+
+export const useUsersData = () => {
+    return useQuery(['users'], () => axios.get('/users'))
+}
+
+export const useUserData = (username: string) => {
+    return useQuery(['user',], () => axios.get(`/users/username/${username}`), {
+        refetchOnMount: false,
+        cacheTime: 0,
+    })
+}
+export const useUserProgress = (username: string) => {
+    return useQuery(['userProgress', username], () => axios.get(`/users/username/${username}/progress`), {
+        refetchOnMount: false,
+        enabled: !!username
+    })
+}
