7fa52558392f4d6952a689ab62285e8b6c92b28e
naresh-khatri <naresh.khatri2345@gmail.com>
2023-01-22 16:51:12 +0530

    Use FontAwesome icons as IconProp type

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -1,4 +1,5 @@
 import { IconButton, useColorMode } from "@chakra-ui/react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faLightbulb, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
@@ -7,9 +8,10 @@ function ThemeToggler() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <IconButton
+      aria-label="Toggle theme"
       icon={
         <FontAwesomeIcon
-          icon={colorMode === "light" ? faMoon : faLightbulb}
+          icon={colorMode === "light" ? faMoon : (faLightbulb as IconProp)}
           height={"1.2rem"}
         />
       }
