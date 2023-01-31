052eba7f587d9dc50678a8d416d7a70c68469c8a
naresh-khatri <naresh.khatri2345@gmail.com>
2023-01-31 16:51:12 +0530

    create new hook to fetch user previous submissions

--- hooks/useExamsData.ts
+++ hooks/useExamsData.ts
@@ -2,22 +2,28 @@ import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "../utils/axios";
 
 export const useExamsData = () => {
-    return useQuery(["exams"], () => axiosInstance.get("/exams"));
+  return useQuery(["exams"], () => axiosInstance.get("/exams"));
 };
 
 export const useExamData = (examSlug: string) => {
-    return useQuery(
-        ["exam", examSlug],
-        () => axiosInstance.get(`/exams/slug/${examSlug}`),
-    );
+  return useQuery(["exam", examSlug], () =>
+    axiosInstance.get(`/exams/slug/${examSlug}`)
+  );
 };
 
-export const useExamProblemsData = ({ examId, enabled, }: {
-    examId: number; enabled: boolean;
-}) => {
-    return useQuery(
-        ["examProblems", examId],
-        () => axiosInstance.get(`/problems/examProblems/${examId}`),
-        { refetchOnMount: false, enabled: enabled }
-    );
+export const useExamProblemsData = ({ examId }: { examId: number }) => {
+  return useQuery(
+    ["examProblems", examId],
+    () => axiosInstance.get(`/problems/examProblems/${examId}`),
+    { refetchOnMount: false, enabled: false }
+  );
+};
+export const useExamSubmissionsData = (examId: number) => {
+  return useQuery(
+    ["userSubmissions", examId],
+    () => {
+      return axiosInstance.get(`/exams/getProgress/${examId}`);
+    },
+    { enabled: !!examId }
+  );
 };
