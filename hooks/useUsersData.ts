693a78d7ae1d7a1b21e01fdd80544d7b5188ea31
naresh-khatri <naresh.khatri2345@gmail.com>
2023-01-31 16:51:12 +0530

    create new hook to fetch user progress

--- hooks/useUsersData.ts
+++ hooks/useUsersData.ts
@@ -1,30 +1,51 @@
-import { useQuery } from "@tanstack/react-query"
-import axios from "../utils/axios"
+import { useQuery } from "@tanstack/react-query";
+import axios from "../utils/axios";
 
 interface User {
-    id: number
-    fullname: string;
-    photoURL: string;
-    rank: number;
-    registeredAt: string;
-    totalMarks: number;
-    username: string
+  id: number;
+  fullname: string;
+  photoURL: string;
+  rank: number;
+  registeredAt: string;
+  totalMarks: number;
+  username: string;
 }
 
 export const useUsersData = () => {
-    return useQuery(['users'], () => axios.get('/users'))
-}
+  return useQuery(["users"], () => axios.get("/users"));
+};
 
 export const useUserData = (username: string) => {
-    return useQuery(['user',], () => axios.get(`/users/username/${username}`), {
-        refetchOnMount: false,
-        cacheTime: 0,
-        enabled: !!username
-    })
-}
+  return useQuery(["user"], () => axios.get(`/users/username/${username}`), {
+    refetchOnMount: false,
+    cacheTime: 0,
+    enabled: !!username,
+  });
+};
 export const useUserProgress = (username: string) => {
-    return useQuery(['userProgress', username], () => axios.get(`/users/username/${username}/progress`), {
-        refetchOnMount: false,
-        enabled: !!username
-    })
-}
+  return useQuery(
+    ["userProgress", username],
+    () => axios.get(`/users/username/${username}/progress`),
+    {
+      refetchOnMount: false,
+      enabled: !!username,
+    }
+  );
+};
+export const useLastSubmissionData = (
+  problemId: number,
+  onSuccessFn: (data: any) => void
+) => {
+  return useQuery(
+    ["lastSubmission", problemId],
+    () => axios.get(`/problems/${problemId}/getLastSubmission/`),
+    {
+      onSuccess: onSuccessFn,
+      enabled: false,
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+      retryOnMount: false,
+      refetchOnMount: false,
+    }
+  );
+};
