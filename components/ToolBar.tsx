b692268feb90f9467e053788a938abdda9c954a3
naresh-khatri <naresh.khatri2345@gmail.com>
2023-02-02 16:51:12 +0530

    show code retrieval btn when onCodeRetrievalModalOpen fn is passed

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -1,4 +1,4 @@
-import { AddIcon } from "@chakra-ui/icons";
+import { AddIcon } from '@chakra-ui/icons'
 import {
   Button,
   Box,
@@ -7,19 +7,19 @@ import {
   useColorModeValue,
   IconButton,
   Tooltip,
-} from "@chakra-ui/react";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faCode, faStepBackward } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from '@chakra-ui/react'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { faCode, faStepBackward } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface ToolBarProps {
-  isLoading?: boolean;
-  runCode?: () => void;
-  lang?: string;
-  setLang: (lang: string) => void;
-  theme?: string;
-  setTheme: (theme: string) => void;
-  onCodeRetrievalModalOpen: () => void;
+  isLoading?: boolean
+  runCode?: () => void
+  lang?: string
+  setLang: (lang: string) => void
+  theme?: string
+  setTheme: (theme: string) => void
+  onCodeRetrievalModalOpen?: () => void
 }
 
 export default function ToolBar({
@@ -28,86 +28,88 @@ export default function ToolBar({
   setTheme,
   onCodeRetrievalModalOpen,
 }: ToolBarProps) {
-  const color = useColorModeValue("black", "white");
+  const color = useColorModeValue('black', 'white')
   return (
     <Box>
-      <HStack p={2} justifyContent="end">
-        <Tooltip label="Retrieve last submitted code">
-          <IconButton
-            aria-label="retrieve last submitted code"
-            icon={<FontAwesomeIcon icon={faCode as IconProp} />}
-            onClick={onCodeRetrievalModalOpen}
-          />
-        </Tooltip>
+      <HStack p={2} justifyContent='end'>
+        {onCodeRetrievalModalOpen && (
+          <Tooltip label='Retrieve last submitted code'>
+            <IconButton
+              aria-label='retrieve last submitted code'
+              icon={<FontAwesomeIcon icon={faCode as IconProp} />}
+              onClick={onCodeRetrievalModalOpen}
+            />
+          </Tooltip>
+        )}
         <Select
-          bg="purple.500"
-          color="white"
+          bg='purple.500'
+          color='white'
           maxW={40}
           value={lang}
           onChange={(e) => {
-            setLang(e.target.value);
-            localStorage.setItem("lang", e.target.value);
+            setLang(e.target.value)
+            localStorage.setItem('lang', e.target.value)
           }}
-          fontWeight="extrabold"
+          fontWeight='extrabold'
         >
-          <option style={{ color: color }} value="py">
+          <option style={{ color: color }} value='py'>
             Python
           </option>
-          <option style={{ color }} value="js">
+          <option style={{ color }} value='js'>
             Javascript
           </option>
-          <option style={{ color }} value="cpp">
+          <option style={{ color }} value='cpp'>
             C++
           </option>
-          <option style={{ color }} value="c">
+          <option style={{ color }} value='c'>
             C
           </option>
-          <option style={{ color }} value="java">
+          <option style={{ color }} value='java'>
             Java
           </option>
         </Select>
         <Select
           maxW={40}
           onChange={(e) => {
-            setTheme(e.target.value);
-            localStorage.setItem("theme", e.target.value);
+            setTheme(e.target.value)
+            localStorage.setItem('theme', e.target.value)
           }}
-          bg="purple.500"
-          color="white"
-          fontWeight={"extrabold"}
+          bg='purple.500'
+          color='white'
+          fontWeight={'extrabold'}
         >
-          <option style={{ color }} value="dracula">
+          <option style={{ color }} value='dracula'>
             Dracula
           </option>
-          <option style={{ color }} value="atomone">
+          <option style={{ color }} value='atomone'>
             Atom One
           </option>
-          <option style={{ color }} value="eclipse">
+          <option style={{ color }} value='eclipse'>
             Eclipse
           </option>
-          <option style={{ color }} value="okaidia">
+          <option style={{ color }} value='okaidia'>
             Okaidia
           </option>
-          <option style={{ color }} value="githubDark">
+          <option style={{ color }} value='githubDark'>
             Github Dark
           </option>
-          <option style={{ color }} value="githubLight">
+          <option style={{ color }} value='githubLight'>
             Github Light
           </option>
-          <option style={{ color }} value="duotoneDark">
+          <option style={{ color }} value='duotoneDark'>
             Duotone Dark
           </option>
-          <option style={{ color }} value="duotoneLight">
+          <option style={{ color }} value='duotoneLight'>
             Duotone Light
           </option>
-          <option style={{ color }} value="xcodeDark">
+          <option style={{ color }} value='xcodeDark'>
             Xcode Dark
           </option>
-          <option style={{ color }} value="xcodeLight">
+          <option style={{ color }} value='xcodeLight'>
             Xcode Light
           </option>
         </Select>
       </HStack>
     </Box>
-  );
+  )
 }
