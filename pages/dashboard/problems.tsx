35eb17107bf8c73a5257eb43fdcfe8f27872af77
naresh-khatri <naresh.khatri2345@gmail.com>
2023-03-25 16:51:12 +0530

    remove useless code

--- pages/dashboard/problems.tsx
+++ pages/dashboard/problems.tsx
@@ -1,55 +1,14 @@
-import { ChevronDownIcon, ChevronUpIcon, CloseIcon } from '@chakra-ui/icons'
 import {
-  Box,
   Button,
   Container,
   Flex,
-  FormControl,
-  FormLabel,
-  HStack,
-  IconButton,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Select,
-  Spacer,
-  Table,
-  TableCaption,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
 } from '@chakra-ui/react'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import ProblemRow from '../../components/admin/ProblemRow'
 import AdminLayout from '../../layout/AdminLayout'
 import { useProblemsData } from '../../hooks/useProblemsData'
 import CustomTable from '../../components/CustomTable'
 
-interface Problem {
-  id: number
-  title: string
-  order: number
-  difficulty: string
-  examId: number
-  exam: {
-    id: number
-    title: string
-    slug: string
-  }
-}
 
-type sortByCol = 'id' | 'order' | 'difficulty' | 'title'
-type sortByOrder = 1 | -1
-interface sortBy {
-  col: sortByCol
-  order: sortByOrder
-}
 
 const COLUMNS = [
   {
