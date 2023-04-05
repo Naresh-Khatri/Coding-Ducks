709e23c17e39c2752314b1c7d823ab7d45480f29
naresh-khatri <naresh.khatri2345@gmail.com>
2023-04-05 16:51:12 +0530

    add finish exam button

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -1,25 +1,50 @@
-import { Flex, HStack, IconButton, Spacer, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faClock, faHome, faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import ThemeToggler from "../components/ThemeToggler";
 import UserProfile from "../components/UserProfile";
 import { useExamSubmissionsData } from "../hooks/useExamsData";
 import Timer from "../components/Timer";
+import { useRouter } from "next/router";
 
 interface MainLayoutProps {
   children: React.ReactNode;
-  title: string;
-  examId: number;
+  examData: {
+    id: number;
+    title: string;
+    isBounded: boolean;
+    endTime: string;
+    slug: string;
+  };
 }
 //this fucntion takes examId to show the marks obtained in the exam
-function MainLayout({ children, title, examId }: MainLayoutProps) {
+function MainLayout({ children, examData }: MainLayoutProps) {
+  const router = useRouter();
+  const { id: examId, title, slug: examSlug } = examData;
   const { data: submissionData } = useExamSubmissionsData(examId);
   const { totalMarks, marksObtained } = submissionData?.data || {
     totalMarks: 0,
     marksObtained: 0,
   };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex direction={"column"} h={"100vh"}>
       <Flex
@@ -66,10 +91,33 @@ function MainLayout({ children, title, examId }: MainLayoutProps) {
           </Text>
           <ThemeToggler />
           <UserProfile />
-
-          {/* <Button variant={"solid"} bg={"red.400"} disabled>
+          <Button variant={"solid"} bg={"red.400"} onClick={onOpen}>
             Finish
-          </Button> */}
+          </Button>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Finish Exam</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Text>
+                  Are you sure you want to finish the exam? You can still come
+                  to complete the exam
+                </Text>
+              </ModalBody>
+              <ModalFooter>
+                <Button variant="ghost">Cancel</Button>
+                <Button
+                  colorScheme="red"
+                  onClick={() => {
+                    router.push("/feedback/" + examSlug);
+                  }}
+                >
+                  Finish
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </HStack>
       </Flex>
       <Flex w={"100vw"} flexGrow={1} overflowY="hidden">
