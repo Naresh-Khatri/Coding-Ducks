3b1c3e6351a4f0f90fad57c1b8495c295d1e779c
naresh-khatri <naresh.khatri2345@gmail.com>
2023-06-06 17:41:29 +0530

    refactor: :art: create new component to show connected clients

---
+++ components/multiplayer/ConnectedUsers.tsx
@@ -0,0 +1,84 @@
+import {
+  Avatar,
+  AvatarBadge,
+  Grid,
+  GridItem,
+  HStack,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import type { FC } from "react";
+import { MyUser } from "../../contexts/userContext";
+
+interface Client {
+  id: string;
+  socketId: string;
+}
+interface ConnectedUsersProps {
+  clients: any[];
+  currentUser: MyUser;
+}
+
+const ConnectedUsers: FC<ConnectedUsersProps> = ({ clients, currentUser }) => {
+  return (
+    <HStack>
+      {Object.keys(clients).map((socketId, i) => (
+        <HStack key={socketId}>
+          <Popover>
+            <PopoverTrigger>
+              <Avatar
+                key={i}
+                name={clients[socketId].username}
+                src={clients[socketId].photoURL}
+                size="sm"
+                colorScheme="green"
+              >
+                <AvatarBadge boxSize="1.25em" bg={"green.500"} />
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader>User Info</PopoverHeader>
+              <PopoverBody>
+                <Grid templateColumns={"repeat(5, 1fr)"}>
+                  <GridItem colSpan={1}>
+                    <Image
+                      width={100}
+                      height={100}
+                      src={clients[socketId].photoURL}
+                      alt="profile photo"
+                      style={{ borderRadius: "50%" }}
+                    />
+                  </GridItem>
+                  <GridItem colSpan={4} mx={2}>
+                    <HStack>
+                      <Text>{clients[socketId].fullname}</Text>
+                      {clients[socketId].id === currentUser.id && (
+                        <Text as="span" color="green.500">
+                          (you)
+                        </Text>
+                      )}
+                    </HStack>
+                    <Text color={"gray.500"}>
+                      @{clients[socketId].username}
+                    </Text>
+                  </GridItem>
+                </Grid>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
+        </HStack>
+      ))}
+    </HStack>
+  );
+};
+
+export default ConnectedUsers;
