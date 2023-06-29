bb41bf3e893037853fbadb2d4003ba580fbfb5aa
naresh-khatri <naresh.khatri2345@gmail.com>
2023-06-29 16:55:12 +0530

    feat: create new hooks to get all user submissions and offload interfaces

--- hooks/useSubmissionsData.ts
+++ hooks/useSubmissionsData.ts
@@ -1,34 +1,16 @@
-import { useQuery } from '@tanstack/react-query'
-import axiosInstance from '../lib/axios'
-export interface User {
-  id?: number
-  fullname: string
-  username: string
-  photoURL: string
-  roll?: string
-}
-
-export interface Submission {
-  id: number
-  User: User
-  code: string
-  examId: number
-  lang: string
-  marks: number
-  tests: any
-  tests_passed: number
-  timestamp: string
-  total_tests: number
-  userId: number
-}
+import { useQuery } from "@tanstack/react-query";
+import axiosInstance from "../lib/axios";
+import { ISubmission } from "../types";
+import { auth } from "../firebase/firebase";
 
 export interface ISubmissionsQuery {
-  skip: number
-  take: number
-  searchTerm: string
-  orderBy: string
-  asc: boolean
+  skip: number;
+  take: number;
+  searchTerm: string;
+  orderBy: string;
+  asc: boolean;
 }
+
 export const useSubmissionsData = ({
   skip,
   take,
@@ -37,7 +19,7 @@ export const useSubmissionsData = ({
   asc,
 }: ISubmissionsQuery) => {
   return useQuery(
-    ['submissions', { skip, take, searchTerm, orderBy, asc }],
+    ["submissions", { skip, take, searchTerm, orderBy, asc }],
     () =>
       axiosInstance.get(`/submissions`, {
         params: {
@@ -48,11 +30,84 @@ export const useSubmissionsData = ({
           asc,
         },
       })
-  )
-}
+  );
+};
 
-export const useSubmissionData = (id: number) => {
-  return useQuery(['submission', id], () =>
-    axiosInstance.get(`/submissions/${id}`)
-  )
-}
+export const useSubmissionData = (submissionId: number, canFetch) => {
+  return useQuery(
+    ["submission", submissionId],
+    async () => {
+      const result = await axiosInstance.get(`/submissions/${submissionId}`);
+      return { data: result.data?.data, status: result.status } as {
+        data: ISubmission;
+        status: number;
+      };
+    },
+    { enabled: canFetch, retryOnMount: false, refetchOnMount: false }
+  );
+};
+
+export const useLastSubmissionData = (
+  problemId: number,
+  onSuccessFn: (data: any) => void
+) => {
+  return useQuery(
+    ["lastSubmission", problemId],
+    () => axiosInstance.get(`/problems/${problemId}/getLastSubmission/`),
+    {
+      onSuccess: onSuccessFn,
+      enabled: false,
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+      retryOnMount: false,
+      refetchOnMount: false,
+    }
+  );
+};
+
+export const useLastSubmissionDataV2 = (
+  problemId: number,
+  lang: string,
+  onSuccessFn: (data: any) => void,
+  onErrorFn: (data: any) => void
+) => {
+  return useQuery(
+    ["lastSubmission", problemId, lang],
+    async () => {
+      const res = await axiosInstance.get(
+        `/submissions/last?problemId=${problemId}&lang=${lang}`
+      );
+      return { data: res.data?.data, status: res.status };
+    },
+    {
+      onSuccess: onSuccessFn,
+      retry: false,
+      onError: onErrorFn,
+      enabled: false,
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+      retryOnMount: false,
+      refetchOnMount: false,
+    }
+  );
+};
+
+export const useCurrentUserSubmissionDataForProblem = (problemId: number) => {
+  return useQuery(
+    ["submission", problemId],
+    async () => {
+      try {
+        const res = await axiosInstance.get(
+          `/submissions/current-user/${problemId}`
+        );
+        return { data: res.data?.data, status: res.status } as {
+          data: ISubmission[] | null;
+          status: number;
+        };
+      } catch (err) {
+        return { data: null, status: 404 };
+      }
+    },
+    { enabled: !!problemId }
+  );
+};
