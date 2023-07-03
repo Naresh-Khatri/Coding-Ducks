820b3e329001a34c17d1458dbc18c198e4a0ccbc
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-03 16:55:12 +0530

    feat: store editor configs

---
+++ data/Editor.ts
@@ -0,0 +1,69 @@
+export const EDITOR_THEMES = [
+  {
+    value: "dracula",
+    label: "Dracula",
+  },
+  {
+    value: "monokai",
+    label: "Monokai",
+  },
+  {
+    value: "github",
+    label: "Github",
+  },
+  {
+    value: "tomorrow",
+    label: "Tomorrow",
+  },
+  {
+    value: "kuroir",
+    label: "Kuroir",
+  },
+  {
+    value: "twilight",
+    label: "Twilight",
+  },
+  {
+    value: "xcode",
+    label: "Xcode",
+  },
+  {
+    value: "textmate",
+    label: "Textmate",
+  },
+  {
+    value: "solarized_dark",
+    label: "Solarized Dark",
+  },
+  {
+    value: "solarized_light",
+    label: "Solarized Light",
+  },
+  {
+    value: "terminal",
+    label: "Terminal",
+  },
+];
+
+export const EDITOR_LANGUAGES = [
+  {
+    label: "Python",
+    value: "py",
+  },
+  {
+    label: "JavaScript",
+    value: "js",
+  },
+  {
+    label: "C++",
+    value: "cpp",
+  },
+  //   {
+  //     label: "C",
+  //     value: "c",
+  //   },
+  {
+    label: "Java",
+    value: "java",
+  },
+];
