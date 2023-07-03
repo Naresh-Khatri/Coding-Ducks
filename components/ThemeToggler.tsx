a4f5cacf7221465ce4885b1c55d8272c9898a4c0
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-03 16:55:12 +0530

    feat: remove unused import

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -2,7 +2,6 @@ import { IconButton, useColorMode, useToast } from "@chakra-ui/react";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faLightbulb, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 
 function ThemeToggler() {
   const { colorMode, toggleColorMode } = useColorMode();
