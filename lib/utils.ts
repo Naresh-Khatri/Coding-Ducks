028427b0bb33deb771ac045f2c3aacd43a8f7f1e
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-04 16:16:42 +0530

    feat: :sparkles: create new file to store constants

---
+++ lib/utils.ts
@@ -0,0 +1,25 @@
+export const DESCRIPTION_TAB_INDEX = 0;
+export const EDITORIOAL_TAB_INDEX = 1;
+export const SOLUTION_TAB_INDEX = 2;
+export const SUBMISSION_TAB_INDEX = 3;
+
+export const lang2Label = {
+  py: "Python",
+  js: "JavaScript",
+  cpp: "C++",
+  c: "C",
+  java: "Java",
+};
+
+export const errorType2Label = {
+  "pre-compile-time": "Pre-Compile Time Error",
+  "compile-time": "Compile Time Error",
+  "run-time": "Runtime Error",
+};
+export const langToAceModes = {
+  py: "python",
+  js: "javascript",
+  cpp: "c_cpp",
+  c: "c_cpp",
+  java: "java",
+};
