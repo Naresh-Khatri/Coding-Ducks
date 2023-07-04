3148e64b41349196a94fe9376305b9d5bd8b573a
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-04 16:09:55 +0530

    feat: :art: create new layout for Problem page

---
+++ layout/ProblemLayout.tsx
@@ -0,0 +1,80 @@
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faHome, faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import ThemeToggler from "../components/ThemeToggler";
+import UserProfile from "../components/UserProfile";
+import Timer from "../components/Timer";
+
+interface ProblemLayoutProps {
+  children: React.ReactNode;
+}
+function ProblemLayout({ children }: ProblemLayoutProps) {
+  return (
+    <Flex direction={"column"} h={"100vh"}>
+      <Flex
+        as="nav"
+        alignItems={"center"}
+        // bg="purple.600"
+        // color={"white"}
+        w={"100vw"}
+        px={2}
+        h={"70px"}
+      >
+        <HStack alignItems={"center"} h={"70px"}>
+          <Link href={"/problems"}>
+            <IconButton
+              aria-label="Go back"
+              bg={"transparent"}
+              icon={
+                <FontAwesomeIcon
+                  height={"1.2rem"}
+                  icon={faLeftLong as IconProp}
+                />
+              }
+            />
+          </Link>
+          <Link href={"/"}>
+            <IconButton
+              aria-label="Go home"
+              bg={"transparent"}
+              icon={
+                <FontAwesomeIcon height={"1.2rem"} icon={faHome as IconProp} />
+              }
+            />
+          </Link>
+        </HStack>
+        <Spacer />
+        {/* <Timer /> */}
+        <Spacer />
+        <HStack>
+          {/* <Timer /> */}
+          <ThemeToggler />
+          <UserProfile />
+        </HStack>
+      </Flex>
+      <Flex w={"100vw"} flexGrow={1} overflowY="hidden">
+        {children}
+      </Flex>
+    </Flex>
+  );
+}
+
+export default ProblemLayout;
