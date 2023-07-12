4fdaae1abbee52e3462b4309da7f8e944f699be1
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-12 16:55:12 +0530

    feat: create new file to store static data about difficulty

---
+++ data/problems.ts
@@ -0,0 +1,34 @@
+export const DIFFICULTIES = [
+  {
+    label: "Tutorial",
+    colorScheme: "blue",
+    value: "tutorial",
+  },
+  {
+    label: "Basic",
+    colorScheme: "teal",
+    value: "basic",
+  },
+  {
+    label: "Easy",
+    colorScheme: "green",
+    value: "easy",
+  },
+  {
+    label: "Medium",
+    colorScheme: "orange",
+    value: "medium",
+  },
+  {
+    label: "Hard",
+    colorScheme: "red",
+    value: "hard",
+  },
+];
+export const DIFFICULTY_TO_COLOR = {
+  tutorial: { color: "cyan", colorScheme: "cyan" },
+  basic: { color: "teal", colorScheme: "teal" },
+  easy: { color: "lightgreen", colorScheme: "green" },
+  medium: { color: "orange", colorScheme: "orange" },
+  hard: { color: "red", colorScheme: "red" },
+};
