c23eca315641da92dc4259f9d81c5a37b8cbbfa9
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-13 18:58:20 +0530

    feat: :art: improve badge component

--- components/problem/DiffBadge.tsx
+++ components/problem/DiffBadge.tsx
@@ -1,25 +1,67 @@
-import { Badge } from "@chakra-ui/react";
+import { Badge, Box, Flex, HStack, Text } from "@chakra-ui/react";
 import React from "react";
+import { IDifficulty } from "../../types";
+import { DIFFICULTY_TO_COLOR } from "../../data/problems";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 interface DiffBadgeProps {
-  difficulty: "easy" | "medium" | "hard";
+  difficulty: IDifficulty;
   size?: "sm" | "md" | "lg";
 }
 
+const diff = {
+  tutorial: {
+    label: "Tutorial",
+    starCount: 0,
+  },
+  easy: {
+    label: "Easy",
+    starCount: 1,
+  },
+  medium: {
+    label: "Medium",
+    starCount: 2,
+  },
+  hard: {
+    label: "Hard",
+    starCount: 3,
+  },
+};
+
 function DiffBadge({ difficulty, size }: DiffBadgeProps) {
   return (
-    <Badge
-      size={size}
-      colorScheme={`${
-        difficulty === "easy"
-          ? "green"
-          : difficulty === "medium"
-          ? "yellow"
-          : "orange"
-      }`}
-    >
-      {difficulty}
-    </Badge>
+    <Flex direction={"column"} alignItems={"center"} justifyContent={"center"}>
+      {difficulty !== "tutorial" && (
+        <HStack>
+          {[1, 2, 3].map((i) => (
+            <FontAwesomeIcon
+              key={i}
+              icon={faStar}
+              color={
+                i <= diff[difficulty].starCount
+                  ? DIFFICULTY_TO_COLOR[difficulty].color
+                  : "gray"
+              }
+              size={size === "sm" ? "xs" : size === "md" ? "sm" : "lg"}
+            />
+          ))}
+        </HStack>
+      )}
+      <Box
+        alignItems={"center"}
+        display={"flex"}
+        justifyContent={"center"}
+        color={DIFFICULTY_TO_COLOR[difficulty].colorScheme}
+      >
+        <Text
+          fontWeight={"extrabold"}
+          color={DIFFICULTY_TO_COLOR[difficulty].color}
+        >
+          {diff[difficulty].label}
+        </Text>
+      </Box>
+    </Flex>
   );
 }
 
