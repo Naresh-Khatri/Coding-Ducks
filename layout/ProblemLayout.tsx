5187876bd9c414f87616aeef8bbacba5f0123706
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-16 18:38:26 +0530

    feat: :iphone: add button to toggle bottom sheet using context

--- layout/ProblemLayout.tsx
+++ layout/ProblemLayout.tsx
@@ -1,32 +1,21 @@
-import {
-  Flex,
-  HStack,
-  IconButton,
-  Spacer,
-} from "@chakra-ui/react";
+import { Button, Flex, HStack, IconButton, Spacer } from "@chakra-ui/react";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faHome, faLeftLong } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCode, faLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import ThemeToggler from "../components/ThemeToggler";
 import UserProfile from "../components/UserProfile";
-import Timer from "../components/Timer";
+import { useContext } from "react";
+import { EditorSettingsContext } from "../contexts/editorSettingsContext";
 
 interface ProblemLayoutProps {
   children: React.ReactNode;
 }
 function ProblemLayout({ children }: ProblemLayoutProps) {
+  const { setBottomSheetIsOpen } = useContext(EditorSettingsContext);
   return (
-    <Flex direction={"column"} h={"100vh"}>
-      <Flex
-        as="nav"
-        alignItems={"center"}
-        // bg="purple.600"
-        // color={"white"}
-        w={"100vw"}
-        px={2}
-        h={"70px"}
-      >
+    <Flex direction={"column"} h={"100dvh"}>
+      <Flex as="nav" alignItems={"center"} w={"100vw"} px={2} h={"70px"}>
         <HStack alignItems={"center"} h={"70px"}>
           <Link href={"/problems"}>
             <IconButton
@@ -40,27 +29,34 @@ function ProblemLayout({ children }: ProblemLayoutProps) {
               }
             />
           </Link>
-          <Link href={"/"}>
-            <IconButton
-              aria-label="Go home"
-              bg={"transparent"}
-              icon={
-                <FontAwesomeIcon height={"1.2rem"} icon={faHome as IconProp} />
-              }
-            />
-          </Link>
         </HStack>
         <Spacer />
-        {/* <Timer /> */}
+        <Link href={"/problems"}>
+          <Button leftIcon={<FontAwesomeIcon icon={faBars as IconProp} />}>
+            Problems
+          </Button>
+        </Link>
         <Spacer />
         <HStack>
-          {/* <Timer /> */}
           <ThemeToggler />
           <UserProfile />
         </HStack>
       </Flex>
       <Flex w={"100vw"} flexGrow={1} overflowY="hidden">
         {children}
+      </Flex>
+      <Flex display={{ base: "flex", md: "none" }}>
+        <Button
+          position={"absolute"}
+          bottom={0}
+          w={"100%"}
+          zIndex={0}
+          colorScheme="purple"
+          leftIcon={<FontAwesomeIcon icon={faCode as IconProp} />}
+          onClick={() => setBottomSheetIsOpen(true)}
+        >
+          Open Editor
+        </Button>
       </Flex>
     </Flex>
   );
