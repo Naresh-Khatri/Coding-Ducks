3d80017f057888a45e1bce3603501b2923c95f68
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-20 21:32:54 +0530

    feat: :zap: support for ssr

--- hooks/useExamsData.ts
+++ hooks/useExamsData.ts
@@ -2,17 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "../lib/axios";
 import { IExam } from "../types";
 
-export const useExamsData = () => {
-  return useQuery(["exams"], async () => {
-    const res = await axiosInstance.get("/exams");
-    return res.data as IExam[];
-  });
+export const useExamsData = ({ initalExamData }) => {
+  return useQuery(
+    ["exams"],
+    async () => {
+      const res = await axiosInstance.get("/exams");
+      return res.data as IExam[];
+    },
+    { initialData: initalExamData }
+  );
 };
 
 export const useExamData = (examSlug: string) => {
-  return useQuery(["exam", examSlug], () =>
-    axiosInstance.get(`/exams/slug/${examSlug}`)
-  );
+  return useQuery(["exam", examSlug], async () => {
+    const { data } = await axiosInstance.get(`/exams/slug/${examSlug}`);
+    console.log(data);
+    return data as IExam;
+  });
 };
 
 export const useExamProblemsData = ({ examId }: { examId: number }) => {
