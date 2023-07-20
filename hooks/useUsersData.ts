0c50b4456f687816eb26dd9ede6f90d44e54e6f0
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-20 21:33:30 +0530

    feat: :sparkles: add types and support for ssr

--- hooks/useUsersData.ts
+++ hooks/useUsersData.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "../lib/axios";
+import { ILeague, IUser } from "../types";
 
 interface User {
   id: number;
@@ -11,24 +12,68 @@ interface User {
   username: string;
 }
 
-export const useUsersData = () => {
-  return useQuery(["users"], () => axios.get("/users"));
+export const useUsersData = ({ initialUsers }) => {
+  return useQuery(
+    ["users"],
+    async () => {
+      const {
+        data: { data: users },
+      } = await axios.get("/users");
+      return users as User[];
+    },
+    { initialData: initialUsers }
+  );
 };
 
-export const useUserData = (username: string) => {
-  return useQuery(["user"], () => axios.get(`/users/username/${username}`), {
-    refetchOnMount: false,
-    cacheTime: 0,
-    enabled: !!username,
-  });
+export const useUserData = ({
+  username,
+  initalUserData,
+}: {
+  username: string;
+  initalUserData: IUser;
+}) => {
+  return useQuery(
+    ["user"],
+    async () => {
+      const { data } = await axios.get(`/users/username/${username}`);
+      return data.data as IUser;
+    },
+    {
+      refetchOnMount: false,
+      cacheTime: 0,
+      enabled: !!username,
+      initialData: initalUserData,
+    }
+  );
 };
-export const useUserProgress = (username: string) => {
+export interface IUserStatsResponse {
+  totalProblemsSolved: number;
+  totalSubCount: number;
+  league: ILeague;
+  points: number;
+  accuracy: number;
+  dailySubmissions: { date: string; count: number }[];
+  streaks: { date: string; count: number }[];
+  longestStreak: { date: string; count: number }[];
+  byExamId: any;
+}
+export const useUserStats = ({
+  username,
+  initalUserStats,
+}: {
+  username: string;
+  initalUserStats: null | IUserStatsResponse;
+}) => {
   return useQuery(
     ["userProgress", username],
-    () => axios.get(`/users/username/${username}/progress`),
+    async () => {
+      const { data } = await axios.get(`/users/username/${username}/stats`);
+      return data.data as IUserStatsResponse;
+    },
     {
       refetchOnMount: false,
       enabled: !!username,
+      initialData: initalUserStats,
     }
   );
 };
