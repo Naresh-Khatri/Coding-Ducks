3cc3a7d437d1a2e21beb9ff34abb56620a55ff51
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-21 22:06:57 +0530

    feat: :mag: add lang='en' in html tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
