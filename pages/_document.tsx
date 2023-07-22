68eece5ae7608eea88b696a63e9c55d5197a1e86
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-22 10:50:19 +0530

    feat: :chart_with_upwards_trend: add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -10,7 +11,23 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
+
       <body>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-VX8GFPR8MZ`}
+        />
+
+        <Script id="google-analytics-script" strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-VX8GFPR8MZ', {
+          page_path: window.location.pathname,
+          });
+    `}
+        </Script>
         <Main />
         <NextScript />
       </body>
