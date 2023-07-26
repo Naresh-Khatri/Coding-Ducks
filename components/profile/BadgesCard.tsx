b268c93fdcce6d5d6be7ce12740811ad03b26470
naresh-khatri <naresh.khatri2345@gmail.com>
2023-07-26 23:48:28 +0530

    refactor: :art: improve code structure and add tyeps

--- components/profile/BadgesCard.tsx
+++ components/profile/BadgesCard.tsx
@@ -13,10 +13,19 @@ import {
   Text,
 } from "@chakra-ui/react";
 import React from "react";
-import { IUser } from "../../types";
+import { IBadge, IUser } from "../../types";
 import Image from "next/image";
 
 function BadgesCard({ userData }: { userData: IUser }) {
+  const badges: IBadge[] = [];
+  if (!userData.isNoob)
+    badges.push({
+      name: "completed tutorial",
+      description: "Completed the tutorial",
+      image:
+        "https://ik.imagekit.io/couponluxury/coding_ducks/badges/tutorial-complete_OIT9T9EMn.svg",
+    });
+
   return (
     <Card
       w={"100%"}
@@ -27,41 +36,52 @@ function BadgesCard({ userData }: { userData: IUser }) {
       p={5}
       mb={5}
       display={"flex"}
-      justifyContent={"space-between"}
+      // justifyContent={"center"}
     >
       <Text fontWeight={"extrabold"}>Badges</Text>
-      {!userData.isNoob ? (
+
+      {badges.length ? (
         <SimpleGrid columns={3} spacing={5} mt={5}>
-          <Popover>
-            <PopoverTrigger>
-              <Flex>
-                <Image
-                  src="https://ik.imagekit.io/couponluxury/coding_ducks/badges/tutorial-complete_OIT9T9EMn.svg"
-                  height={50}
-                  width={50}
-                  alt="badge"
-                />
-                {/* <Text>completed tutorial</Text> */}
-              </Flex>
-            </PopoverTrigger>
-            <Portal>
-              <PopoverContent w={"200px"}>
-                <PopoverArrow />
-                <PopoverCloseButton />
-                <PopoverBody>Completed Tutorial!</PopoverBody>
-              </PopoverContent>
-            </Portal>
-          </Popover>
+          {badges.map((badge) => (
+            <Badge key={badge.name} badge={badge} />
+          ))}
         </SimpleGrid>
       ) : (
-        <Text color={"whiteAlpha.400"} textAlign={"center"}>
-          No badges yet 😢
-          <br /> Complete the basic to get your first badge!
-        </Text>
+        <Box mt={"50px"}>
+          <Text color={"whiteAlpha.400"} textAlign={"center"}>
+            No badges yet 😢
+            <br /> Complete the basic to get your first badge!
+          </Text>
+        </Box>
       )}
-      <Box></Box>
+      {/* <Box></Box> */}
     </Card>
   );
 }
+
+const Badge = ({ badge }: { badge: IBadge }) => {
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Flex w={'fit-content'}>
+          <Image
+            src={badge.image}
+            height={50}
+            width={50}
+            alt="badge"
+            style={{ cursor: "pointer" }}
+          />
+        </Flex>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent w={"200px"}>
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverBody>{badge.description}</PopoverBody>
+        </PopoverContent>
+      </Portal>
+    </Popover>
+  );
+};
 
 export default BadgesCard;
