fe2ca04fd2adfb15c6bcd173438bed3c8c2e85cb
naresh-khatri <naresh.khatri2345@gmail.com>
2023-08-10 16:55:12 +0530

    style: introduce new diff lvl 'very easy'

--- data/problems.ts
+++ data/problems.ts
@@ -5,6 +5,11 @@ export const DIFFICULTIES = [
     value: "tutorial",
   },
   {
+    label: "Very Easy",
+    colorScheme: "teal",
+    value: "veryEasy",
+  },
+  {
     label: "Basic",
     colorScheme: "teal",
     value: "basic",
@@ -29,6 +34,7 @@ export const DIFFICULTY_TO_COLOR = {
   tutorial: { color: "cyan", colorScheme: "cyan", label: "Basic" },
   basic: { color: "teal", colorScheme: "teal", label: "Basic" },
   easy: { color: "lightgreen", colorScheme: "green", label: "Easy" },
+  veryEasy: { color: "lightgreen", colorScheme: "green", label: "Very Easy" },
   medium: { color: "orange", colorScheme: "orange", label: "Medium" },
   hard: { color: "red", colorScheme: "red", label: "Hard" },
 };
