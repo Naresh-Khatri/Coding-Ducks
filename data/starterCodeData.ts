e934a5a2c8c3b4ceb6f836ad7ad6fbe8265405c1
naresh-khatri <naresh.khatri2345@gmail.com>
2023-08-10 16:55:12 +0530

    fix: reduce starter code to one line of comment

--- data/starterCodeData.ts
+++ data/starterCodeData.ts
@@ -12,142 +12,139 @@ export const INITIAL_STARTER_CODES = [
   {
     langLabel: "Python",
     lang: "py",
-    code: `#User function Template for python3
+    code: "# Code here",
+    //     code: `#User function Template for python3
 
+    // class Solution:
+    //     def <fun_name> (self, N):
+    //         # code here
 
-class Solution:
-    def <fun_name> (self, N):
-        # code here
+    // #{ 🦆
+    //  # Driver Code Starts
+    // #Initial Template for Python 3
 
+    // if __name__ == '__main__':
+    //     N = int(input())
 
-#{ 🦆
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__': 
-    N = int(input())
-       
-
-    ob = Solution()
-    print(ob.<fun_nam>(N))
-# } Driver Code Ends🦆`,
+    //     ob = Solution()
+    //     print(ob.<fun_nam>(N))
+    // # } Driver Code Ends🦆`,
   },
   {
     langLabel: "JavaScript",
     lang: "js",
-    code: `//{ Driver Code Starts🦆
-//Initial Template for javascript
+    code: "// Code here",
+    //     code: `//{ Driver Code Starts🦆
+    // //Initial Template for javascript
 
-'use strict';
+    // 'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+    // process.stdin.resume();
+    // process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
+    // let inputString = '';
+    // let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+    // process.stdin.on('data', inputStdin => {
+    //     inputString += inputStdin;
+    // });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
+    // process.stdin.on('end', _ => {
+    //     inputString = inputString.trim().split('\n').map(string => {
+    //         return string.trim();
+    //     });
 
-function readLine() {
-    return inputString[currentLine++];
-}
+    //     main();
+    // });
 
-function main() {
-    let t = parseInt(readLine());
-    let i = 0;
-    for(;i<t;i++)
-    {
-        let n = parseInt(readLine());
-        let arr = readLine().trim().split(" ").map((x) => parseInt(x));
-        let obj = new Solution();
-        let res=obj.<fun_name>(arr);
-        console.log(res);
-    }
-}
+    // function readLine() {
+    //     return inputString[currentLine++];
+    // }
 
-// } Driver Code Ends 🦆
+    // function main() {
+    //     let t = parseInt(readLine());
+    //     let i = 0;
+    //     for(;i<t;i++)
+    //     {
+    //         let n = parseInt(readLine());
+    //         let arr = readLine().trim().split(" ").map((x) => parseInt(x));
+    //         let obj = new Solution();
+    //         let res=obj.<fun_name>(arr);
+    //         console.log(res);
+    //     }
+    // }
 
+    // // } Driver Code Ends 🦆
 
-//User function Template for javascript
+    // //User function Template for javascript
 
-/**
- * @param {number[]} arr
- * @return {number}
-*/
+    // /**
+    //  * @param {number[]} arr
+    //  * @return {number}
+    // */
 
-class Solution {
+    // class Solution {
 
-    <fun_name>(arr){
-        //code here
-    }
-}`,
+    //     <fun_name>(arr){
+    //         //code here
+    //     }
+    // }`,
   },
   {
     langLabel: "C++",
     lang: "cpp",
-    code: `//{ Driver Code Starts🦆
-#include<bits/stdc++.h> 
-using namespace std;
+    code: "// Code here",
+    //     code: `//{ Driver Code Starts🦆
+    // #include<bits/stdc++.h>
+    // using namespace std;
 
-// } Driver Code Ends🦆
-class Solution{
-public:
-    int <fun_name>(int N){
-        //code here
-    }
-};
+    // // } Driver Code Ends🦆
+    // class Solution{
+    // public:
+    //     int <fun_name>(int N){
+    //         //code here
+    //     }
+    // };
 
-//{ Driver Code Starts.🦆
-int main() 
-{ 
-    int N;
-    cin>>N;
-    Solution ob;
-    cout << ob.<fun_name>(N) << endl;
-    return 0; 
-}
-// } Driver Code Ends🦆`,
+    // //{ Driver Code Starts.🦆
+    // int main()
+    // {
+    //     int N;
+    //     cin>>N;
+    //     Solution ob;
+    //     cout << ob.<fun_name>(N) << endl;
+    //     return 0;
+    // }
+    // // } Driver Code Ends🦆`,
   },
   {
     langLabel: "Java",
     lang: "java",
-    code: `//{ Driver Code Starts🦆
-//Initial Template for Java
+    code: "// Code here",
+    //     code: `//{ Driver Code Starts🦆
+    // //Initial Template for Java
 
+    // import java.io.*;
+    // import java.util.*;
 
-import java.io.*;
-import java.util.*;
+    // class CodingDucks
+    // {
+    //     public static void main(String args[])throws IOException
+    //     {
+    //         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+    //         int N = Integer.parseInt(read.readLine());
+    //         Solution ob = new Solution();
+    //         System.out.println(ob.<fun_name>(N));
+    //     }
+    // }
+    // // } Driver Code Ends🦆
 
-class CodingDucks
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(read.readLine());
-        Solution ob = new Solution();
-        System.out.println(ob.<fun_name>(N));
-    }
-}
-// } Driver Code Ends🦆
+    // //User function Template for Java
 
-
-//User function Template for Java
-
-
-class Solution{
-    static int fun_name(int N){
-        // code here
-    }
-}`,
+    // class Solution{
+    //     static int fun_name(int N){
+    //         // code here
+    //     }
+    // }`,
   },
 ] as IStarterCode[];
