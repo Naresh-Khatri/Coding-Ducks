82b6415ed3866d2bd876620d4392b838bf3e8525
naresh-khatri <naresh.khatri2345@gmail.com>
2023-08-13 16:55:12 +0530

    feat: create a util function 'pointsToLeague'

--- lib/utils.ts
+++ lib/utils.ts
@@ -23,3 +23,13 @@ export const langToAceModes = {
   c: "c_cpp",
   java: "java",
 };
+
+export const pointsToLeague = (points: number) => {
+  if (points < 10) return { id: "noob", label: "Noob" };
+  else if (points < 50) return { id: "beginner", label: "Beginner" };
+  else if (points < 100) return { id: "intermediate", label: "Intermediate" };
+  else if (points < 200) return { id: "advance", label: "Advance" };
+  else if (points < 300) return { id: "expert", label: "Expert" };
+  else if (points < 450) return { id: "master", label: "Master" };
+  else return { id: "grandmaster", label: "Grand Master" };
+};
