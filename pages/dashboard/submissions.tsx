f338add22194e152a7bbeabcd26decd0270fb1ab
naresh-khatri <naresh.khatri2345@gmail.com>
2023-08-14 16:55:12 +0530

    feat: use custom RQ hook to fetch data and show spinner

--- pages/dashboard/submissions.tsx
+++ pages/dashboard/submissions.tsx
@@ -1,21 +1,36 @@
-import { Button, Container, Flex } from '@chakra-ui/react'
-import Link from 'next/link'
-import AdminLayout from '../../layout/AdminLayout'
-import SubmissionsTable from '../../components/SubmissionsTable'
+import { Button, Container, Flex } from "@chakra-ui/react";
+import Link from "next/link";
+import AdminLayout from "../../layout/AdminLayout";
+import SubmissionsTable from "../../components/SubmissionsTable";
+import { useSubmissionsData } from "../../hooks/useSubmissionsData";
+import { useState } from "react";
+import { ISubmissionsQuery } from "../../types";
 
 function ProblemPage() {
+  const [query, setQuery] = useState<ISubmissionsQuery>({
+    skip: 0,
+    take: 10,
+    searchTerm: "",
+    orderBy: "id",
+    asc: false,
+  });
+  const { data, isLoading, error, refetch } = useSubmissionsData(query);
+
   return (
     <AdminLayout>
-      <Container maxW='full' overflowY={'scroll'}>
-        <Flex m={10} justify='space-between'>
-          <Link href='/dashboard/add-problem'>
-            <Button bg='green.400'>Add Problem</Button>
-          </Link>
-        </Flex>
-        <SubmissionsTable />
+      <Container  p={10} maxW="full" overflowY={"scroll"}>
+        {isLoading ? (
+          "Loading..."
+        ) : (
+          <SubmissionsTable
+            query={query}
+            setQuery={setQuery}
+            subsData={data.data}
+          />
+        )}
       </Container>
     </AdminLayout>
-  )
+  );
 }
 
-export default ProblemPage
+export default ProblemPage;
