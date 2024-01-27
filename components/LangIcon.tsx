b005bd4cda8fb05ccdd5c5e5622b3cffde3a208e
naresh-khatri <naresh.khatri2345@gmail.com>
2024-01-27 09:53:08 +0530

    feat: utility component for language icons

---
+++ components/LangIcon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Lang } from "../types";
+import FAIcon from "./FAIcon";
+import {
+  faCuttlefish,
+  faJava,
+  faPython,
+  faSquareJs,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+function LangIcon({ lang, height }: { lang: Lang; height: string }) {
+  if (lang === "py") return <FAIcon icon={faPython} height={height} />;
+  if (lang === "js") return <FAIcon icon={faSquareJs} height={height} />;
+  if (lang === "java") return <FAIcon icon={faJava} height={height} />;
+  if (lang === "c") return <FAIcon icon={faCuttlefish} height={height} />;
+  if (lang === "cpp")
+    return (
+      <Flex alignItems={"center"}>
+        <FAIcon icon={faCuttlefish} height={height} />
+        <Text fontWeight={"extrabold"} fontSize={"2xl"}>
+          ++
+        </Text>
+      </Flex>
+    );
+}
+
+export default LangIcon;
