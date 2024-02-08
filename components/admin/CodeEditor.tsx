9348ec8002b21d68281fec3d48bf3d93249082ba
naresh-khatri <naresh.khatri2345@gmail.com>
2024-02-08 20:34:23 +0530

    feat: :sparkles: use BaseAceEditor component

--- components/admin/CodeEditor.tsx
+++ components/admin/CodeEditor.tsx
@@ -1,17 +1,5 @@
-import AceEditor from "react-ace";
-
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/mode-python";
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/mode-c_cpp";
-
-import "ace-builds/src-noconflict/theme-dracula";
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/theme-terminal";
-
-import "ace-builds/src-noconflict/ext-language_tools";
 import { FC } from "react";
+import BaseAceEditor from "../editors/BaseAceEditor";
 
 interface CodeEditorProps {
   value: string;
@@ -30,7 +18,7 @@ const CodeEditor: FC<CodeEditorProps> = ({
 }) => {
   return (
     <>
-      <AceEditor
+      <BaseAceEditor
         mode={lang}
         value={value}
         onChange={onChange}
