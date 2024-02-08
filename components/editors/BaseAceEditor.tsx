19d9253f88383d71983cc9968a2fbe8ed5645f9d
naresh-khatri <naresh.khatri2345@gmail.com>
2024-02-08 20:27:51 +0530

    feat: :sparkles: add more themes and langs

--- components/editors/BaseAceEditor.tsx
+++ components/editors/BaseAceEditor.tsx
@@ -1,6 +1,14 @@
 import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-dracula";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-c_cpp";
+
 import "ace-builds/src-noconflict/ext-language_tools";
+
+import "ace-builds/src-noconflict/theme-dracula";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-terminal";
 
 import AceEditor from "react-ace";
 
