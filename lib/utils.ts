d8e93e187068dfcfceb32bf47f483f075f3ad413
naresh-khatri <naresh.khatri2345@gmail.com>
2024-03-22 20:40:57 +0530

    feat: :sparkles: create new function to get random colors

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { IDirectory, IFile } from "./socketio/socketEventTypes";
+
 export const DESCRIPTION_TAB_INDEX = 0;
 export const EDITORIOAL_TAB_INDEX = 1;
 export const SOLUTION_TAB_INDEX = 2;
@@ -40,4 +42,45 @@ export const isTouchScreen = () => {
     navigator.maxTouchPoints > 0 ||
     navigator.maxTouchPoints > 0
   );
+};
+export const debounce = (fun: Function, interval: number = 2000) => {
+  let timer;
+  return (...args) => {
+    const context = this;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fun.apply(context, args);
+    }, interval);
+  };
+};
+export const getFileFromFS = (
+  dir: IDirectory,
+  fileId: number
+): IFile | null => {
+  if (dir.files)
+    for (let file of dir.files) {
+      if (file.id === fileId) return file;
+    }
+  if (dir.childDirs)
+    for (let childDir of dir.childDirs) {
+      const fileFound = getFileFromFS(childDir, fileId);
+      return fileFound;
+    }
+  return null;
+};
+
+export const COLORS = [
+  { name: "purple", value: "#9F7AEA" },
+  { name: "pink", value: "#ED64A6" },
+  { name: "green", value: "#48BB78" },
+  { name: "yellow", value: "#ECC94B" },
+  { name: "orange", value: "#ED8936" },
+  { name: "red", value: "#F56565" },
+  { name: "teal", value: "#38B2AC" },
+  { name: "blue", value: "#4299E1" },
+  { name: "cyan", value: "#0BC5EA" },
+];
+export const getRandColor = () => {
+  return COLORS[Math.floor(Math.random() * COLORS.length)];
 };
