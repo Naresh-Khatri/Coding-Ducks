27fe8dc700043d15b4a2615abbc177866c0295a2
naresh-khatri <naresh.khatri2345@gmail.com>
2024-03-22 20:41:29 +0530

    feat: :sparkles: create new function to get time in {time} ago format

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -17,3 +17,36 @@ export const formatTime = (timestamp: string) => {
     date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
   return `${hours}:${minutes}:${seconds}`;
 };
+
+// function to get time ago
+export const getTimeAgo = (timestamp: Date) => {
+  const now = new Date();
+  const then = new Date(timestamp);
+  const secDiff = (now.getTime() - then.getTime()) / 1000;
+
+  if (secDiff < 30) {
+    return "just now";
+  } else if (secDiff < 45) {
+    return "a few seconds ago";
+  } else if (secDiff < 90) {
+    return "a minute ago";
+  } else if (secDiff < 2700) {
+    return `${Math.floor(secDiff / 60)}m ago`;
+  } else if (secDiff < 5400) {
+    return "an hour ago";
+  } else if (secDiff < 86400) {
+    return `${Math.floor(secDiff / 3600)}h ago`;
+  } else if (secDiff < 172800) {
+    return "a day ago";
+  } else if (secDiff < 2592000) {
+    return `${Math.floor(secDiff / 86400)}d ago`;
+  } else if (secDiff < 5184000) {
+    return "a month ago";
+  } else if (secDiff < 31104000) {
+    return `${Math.floor(secDiff / 2592000)} months ago`;
+  } else if (secDiff < 62208000) {
+    return "a year ago";
+  } else {
+    return `${Math.floor(secDiff / 31104000)}y ago`;
+  }
+};
