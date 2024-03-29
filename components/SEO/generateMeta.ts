44e354ccd5823ba18139339b08e1df9593e04398
naresh-khatri <naresh.khatri2345@gmail.com>
2024-03-29 14:50:32 +0530

    feat: :mag: create a function to generate metadata

---
+++ components/SEO/generateMeta.ts
@@ -0,0 +1,70 @@
+import { Metadata } from "next";
+import { SetMetaProps } from "./SetMeta";
+import { metadata } from "app/layout";
+
+const generateMeta = async ({
+  title,
+  description,
+  keywords,
+  image,
+  url,
+}: SetMetaProps): Promise<Metadata> => {
+  const _title =
+    title || "Coding Ducks - Practice Coding Problems in Multiple Languages";
+  const _description =
+    description ||
+    `Coding Ducks is a platform where you can enhance your coding skills by solving a variety of coding problems. Practice and improve your programming abilities in Python, JavaScript, C++, and Java with our collection of coding challenges.`;
+  const _keywords =
+    keywords ||
+    `coding problems, coding challenges, programming practice, Python, JavaScript, C++, Java, coding website, programming exercises, algorithmic problem solving`;
+  const _image =
+    image ||
+    "https://ik.imagekit.io/couponluxury/coding_ducks/og_logo_jpRQg8f-Z.png";
+  const _url = url || "https://www.codingducks.live/";
+
+  let metaData: Metadata = {};
+
+  /**
+   * TITLE
+   */
+
+  metaData = {
+    ...metaData,
+    title: _title,
+    openGraph: { ...metaData.openGraph, title: _title },
+  };
+  /**
+   * DESCRIPTION
+   */
+  metaData = {
+    ...metaData,
+    description: _description,
+    openGraph: { ...metaData.openGraph, description: _description },
+  };
+
+  /**
+   * KEYWORDS
+   */
+  metaData = {
+    ...metaData,
+    keywords: _keywords,
+  };
+
+  /**
+   * IMAGE
+   */
+  metaData = {
+    ...metaData,
+    openGraph: { ...metaData.openGraph, images: _image },
+  };
+
+  /**
+   * URL
+   */
+  metaData = {
+    ...metaData,
+    alternates: { ...metaData.alternates, canonical: _url },
+  };
+  return metaData;
+};
+export default generateMeta;
