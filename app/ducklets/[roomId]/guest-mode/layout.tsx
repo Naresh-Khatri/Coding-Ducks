eebb11519cbd9dd61a70a903274ae50dc56ea995
naresh-khatri <naresh.khatri2345@gmail.com>
2024-03-29 14:53:40 +0530

    feat: :sparkles: redirect to edit if user is owner

--- app/ducklets/[roomId]/guest-mode/layout.tsx
+++ app/ducklets/[roomId]/guest-mode/layout.tsx
@@ -1,52 +1,26 @@
 "use client";
 
-import { use } from "react";
+import { use, useEffect } from "react";
 import { userContext } from "../../../../contexts/userContext";
 import { useParams } from "next/navigation";
-import { useRoomData } from "../../../../hooks/useRoomsData";
-import { IUser } from "../../../../types";
-import { ISocketRoom } from "../../../../lib/socketio/socketEvents";
-import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  Flex,
-  HStack,
-  IconButton,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-  Portal,
-  Text,
-  Tooltip,
-  VStack,
-  useDisclosure,
-} from "@chakra-ui/react";
-import FAIcon from "../../../../components/FAIcon";
-import {
-  faBars,
-  faCloud,
-  faEye,
-  faEyeSlash,
-  faSlash,
-  faTriangleExclamation,
-} from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
-import ShareMenu from "../../../../components/ducklets/ShareMenu";
-import { InfoIcon, WarningIcon, WarningTwoIcon } from "@chakra-ui/icons";
-import Image from "next/image";
-import { getTimeAgo } from "../../../../lib/formatDate";
-import DucksletsList from "../../../../components/ducklets/DucksletsList";
+import { getRoom, useRoomData } from "../../../../hooks/useRoomsData";
 import DuckletsNavbar from "../../../../components/ducklets/Navbar";
+// import generateMeta from "components/SEO/generateMeta";
+import { Flex } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
 
+// export async function generateMetadata({ params }) {
+//   const { roomId } = params;
+//   try {
+//     const data = await getRoom({ id: roomId });
+//     const metaData = generateMeta({
+//       title: `${data.data.name}(guest) | Ducklets`,
+//       description: `Join ${data.data.name}, created By ${data.data.owner?.fullname}\n ${data.data.description}`,
+//       url: `https://www.codingducks.live/ducklets/${roomId}`,
+//     });
+//     return metaData;
+//   } catch (err) {}
+// }
 export default function GuestModeLayout({
   children,
 }: {
@@ -55,7 +29,17 @@ export default function GuestModeLayout({
   const { user, userLoaded } = use(userContext);
   const { roomId } = useParams() as { roomId: string };
   const { data: currRoom, isLoading } = useRoomData({ id: +roomId });
-  if (!currRoom) return null;
+  const router = useRouter();
+  useEffect(() => {
+    // if current user is owner or in allowed list then redirect to edit
+    const userIsAllowedToEdit =
+      user &&
+      currRoom &&
+      (user.id === currRoom.ownerId ||
+        currRoom.allowedUsers?.some((u) => u.id === user.id));
+    if (userIsAllowedToEdit) router.push(`/ducklets/${roomId}`);
+  }, [userLoaded]);
+  if (!currRoom || !userLoaded) return null;
   return (
     <Flex direction={"column"} h={"100vh"}>
       <DuckletsNavbar user={user} userLoaded={userLoaded} room={currRoom} />
