5f70cbd85abb23092e4c69c0c4156bccc7284de0
naresh-khatri <naresh.khatri2345@gmail.com>
2024-03-29 14:49:59 +0530

    feat: :mag: add default metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,24 @@
 import { Metadata } from "next";
 import Providers from "./providers";
 
-export const metadata: Metadata = {
-  title: "Home",
-  description: "Welcome to Next.js",
-};
 import "../styles/globals.css";
 import "../styles/split.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://acme.com"),
+  title: "Coding Ducks - Enhance Your Coding Skills with Fun Challenges",
+  description:
+    "Join Coding Ducks and take your coding skills to new heights. Solve coding challenges, practice programming in Python, JavaScript, C++, and Java, and level up your coding expertise.",
+  keywords:
+    "coding challenges, programming practice, Python, JavaScript, C++, Java, coding skills, coding platform, algorithmic problem solving",
+  authors: [{ name: "naresh", url: "https://www.github.com/naresh-khatri" }],
+  openGraph: {
+    title: "Ducklets | CodingDucks",
+    description: "Welcome to Ducklets!",
+    url: "https://www.codingducks.live/",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
