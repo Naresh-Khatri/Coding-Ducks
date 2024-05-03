3b654478fcf658df23bc8f4836139381781a8df4
naresh-khatri <naresh.khatri2345@gmail.com>
2024-05-03 14:13:36 +0530

    feat: :sparkles: add preview to ducklets

--- app/ducklets/(CusomLayout)/page.tsx
+++ app/ducklets/(CusomLayout)/page.tsx
@@ -42,6 +42,7 @@ import { getTimeAgo } from "lib/formatDate";
 import UserAvatar from "components/utils/UserAvatar";
 import { AddIcon, CloseIcon, DeleteIcon } from "@chakra-ui/icons";
 import CreateDuckletModal from "components/ducklets/CreateDuckletModal";
+import Image from "next/image";
 
 function DuckletsPage() {
   const { user, userLoaded } = use(userContext);
@@ -87,13 +88,13 @@ function DuckletsPage() {
         {userLoaded && !user ? (
           <Text>Log in to view your ducklets</Text>
         ) : myRoomsLoading ? (
-          <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} spacing={10}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
             {Array.from({ length: 7 }).map((_, i) => (
               <Skeleton w={"100%"} h={"128px"} key={i} borderRadius={"5px"} />
             ))}
           </SimpleGrid>
         ) : (
-          <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} spacing={10}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
             {myRooms?.map((room) => (
               <RoomCard
                 room={room}
@@ -111,13 +112,13 @@ function DuckletsPage() {
         </Text>
         <Divider mb={5} />
         {otherRoomsLoading ? (
-          <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} spacing={10}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
             {Array.from({ length: 11 }).map((_, i) => (
               <Skeleton w={"100%"} h={"128px"} key={i} borderRadius={"5px"} />
             ))}
           </SimpleGrid>
         ) : (
-          <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} spacing={10}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
             {filteredOtherRooms?.map((room) => (
               <RoomCard room={room} key={room.id} />
             ))}
@@ -188,126 +189,151 @@ const RoomCard = ({
   };
   return (
     <SlideFade in={true} offsetY="20px">
-      <Card
-        direction={{ base: "column", sm: "row" }}
-        variant="outline"
-        _hover={{ border: "1px solid #9d4edd", transition: "border .2s" }}
-        position={"relative"}
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
-      >
-        {isMine && isHovering && (
-          <SlideFade
-            in={true}
-            offsetY="20px"
-            style={{ position: "absolute", right: "-3px", top: "-3px" }}
-          >
-            <IconButton
-              aria-label="delete ducklet"
-              onClick={onOpen}
-              colorScheme="red"
-              icon={<DeleteIcon />}
-              size={"sm"}
-            />
-          </SlideFade>
-        )}
-        {isOpen && (
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Modal Title</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                Are you sure you want to delete this Ducklet?
-              </ModalBody>
-
-              <ModalFooter>
-                <Button variant="ghost">No</Button>
-                <Button
-                  colorScheme="red"
-                  mr={3}
-                  onClick={handleDuckletRemove}
-                  isLoading={isDeleteingRoom}
-                >
-                  Delete
-                </Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-        )}
-        <Stack w={"full"}>
-          <CardBody pb={0}>
-            <HStack justifyContent={"space-between"} align={"start"}>
-              <VStack alignItems={"start"}>
-                <Heading size="md">{room.name}</Heading>
-
-                <HStack>
-                  {room.owner && (
-                    <UserAvatar
-                      src={room.owner.photoURL || ""}
-                      name={room.owner.username}
-                      width="20px"
-                      height="20px"
-                      w={20}
-                      h={20}
-                      alt="profile photo"
-                    />
-                  )}
-                  <Text color={"whiteAlpha.500"}>
-                    {/* {isMine ? "Me" : room.owner && room.owner.username} */}
-                    {room.owner && room.owner.username}
-                  </Text>
-                </HStack>
-              </VStack>
-              <Box>
-                {isMine && (
-                  <HStack>
-                    <FAIcon
-                      icon={room.isPublic ? faGlobe : faLock}
-                      style={{ color: "#555" }}
-                    />
-                    <Text color={"whiteAlpha.500"}>
-                      {room.isPublic ? "Public" : "Private"}
-                    </Text>
-                  </HStack>
-                )}
-              </Box>
-              {/* <LangIcon lang={room.lang} height="2rem" /> */}
-              {/* <CodePreview code={room.content} lang={room.lang} /> */}
-            </HStack>
-          </CardBody>
-
-          <CardFooter w={"full"} pt={0}>
-            <HStack
-              justifyContent={"space-between"}
-              alignItems={"end"}
-              w={"full"}
+      {/* CHANGE IT WITH NEXT LINK LATER */}
+      <a href={`/ducklets/${room.id}`}>
+        <Card
+          direction={{ base: "column", sm: "row" }}
+          variant="outline"
+          _hover={{
+            border: "2px solid #9d4edd",
+            transition: "border .2s",
+            transform: "scale(1.02) rotate(-1deg)",
+          }}
+          position={"relative"}
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
+        >
+          {isMine && isHovering && (
+            <SlideFade
+              in={true}
+              offsetY="20px"
+              style={{ position: "absolute", right: "-3px", top: "-3px" }}
             >
-              <VStack alignItems={"start"}>
-                {room.clients && room.clients.length > 0 && (
-                  <AvatarGroup>
-                    {room.clients.map((client, index) => (
-                      <Avatar
-                        key={index}
-                        name={client.username}
-                        src={client.photoURL}
-                      />
-                    ))}
-                  </AvatarGroup>
-                )}
-                <Text color={"whiteAlpha.500"}>
-                  {room.updatedAt ? getTimeAgo(room.updatedAt) : "NA"}
-                </Text>
-              </VStack>
-              <a href={`/ducklets/${room.id}`}>
-                <Button variant="solid" colorScheme="purple">
+              <IconButton
+                aria-label="delete ducklet"
+                onClick={onOpen}
+                colorScheme="red"
+                icon={<DeleteIcon />}
+                size={"sm"}
+              />
+            </SlideFade>
+          )}
+          {isOpen && (
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  Are you sure you want to delete this Ducklet?
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button variant="ghost">No</Button>
+                  <Button
+                    colorScheme="red"
+                    mr={3}
+                    onClick={handleDuckletRemove}
+                    isLoading={isDeleteingRoom}
+                  >
+                    Delete
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          )}
+          <Stack w={"full"}>
+            <CardBody p={0}>
+              <Image
+                src={room.previewImage || ""}
+                alt="room preview"
+                width={200}
+                height={200}
+                style={{
+                  width: "300px",
+                  height: "auto",
+                  aspectRatio: "16/9",
+                  borderRadius: "3px",
+                }}
+              />
+            </CardBody>
+
+            <CardFooter w={"full"} pt={0}>
+              <VStack w={"full"}>
+                <HStack
+                  justifyContent={"space-between"}
+                  align={"start"}
+                  w="100%"
+                >
+                  <VStack alignItems={"start"}>
+                    <Heading size="md">{room.name}</Heading>
+
+                    <HStack>
+                      {room.owner && (
+                        <UserAvatar
+                          src={room.owner.photoURL || ""}
+                          name={room.owner.username}
+                          width="20px"
+                          height="20px"
+                          w={20}
+                          h={20}
+                          alt="profile photo"
+                        />
+                      )}
+                      <Text color={"whiteAlpha.500"}>
+                        {/* {isMine ? "Me" : room.owner && room.owner.username} */}
+                        {room.owner && room.owner.username}
+                      </Text>
+                    </HStack>
+                  </VStack>
+                  {/* <LangIcon lang={room.lang} height="2rem" /> */}
+                  {/* <CodePreview code={room.content} lang={room.lang} /> */}
+                </HStack>
+                <HStack
+                  justifyContent={"space-between"}
+                  alignItems={"end"}
+                  w={"full"}
+                >
+                  <VStack alignItems={"start"}>
+                    {room.clients && room.clients.length > 0 && (
+                      <AvatarGroup>
+                        {room.clients.map((client, index) => (
+                          <Avatar
+                            key={index}
+                            name={client.username}
+                            src={client.photoURL}
+                          />
+                        ))}
+                      </AvatarGroup>
+                    )}
+                    <Text color={"whiteAlpha.500"}>
+                      {room.updatedAt ? getTimeAgo(room.updatedAt) : "NA"}
+                    </Text>
+                  </VStack>
+                  <Box>
+                    {isMine && (
+                      <HStack>
+                        <FAIcon
+                          icon={room.isPublic ? faGlobe : faLock}
+                          style={{ color: "#555" }}
+                        />
+                        <Text color={"whiteAlpha.500"}>
+                          {room.isPublic ? "Public" : "Private"}
+                        </Text>
+                      </HStack>
+                    )}
+                  </Box>
+                  {/* <a href={`/ducklets/${room.id}`}>
+                <Button variant="solid" colorScheme="purple" size={"sm"}>
                   Open
                 </Button>
-              </a>
-            </HStack>
-          </CardFooter>
-        </Stack>
-      </Card>
+              </a> */}
+                </HStack>
+              </VStack>
+            </CardFooter>
+          </Stack>
+        </Card>
+      </a>
     </SlideFade>
   );
 };
