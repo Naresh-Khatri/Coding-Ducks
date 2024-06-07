bbc9f92a62eeb00db311ac1139ec4276d95df103
naresh-khatri <naresh.khatri2345@gmail.com>
2024-06-07 19:19:16 +0530

    feat: :sparkles: add lang tabs for playground

--- components/ToolBar.tsx
+++ components/ToolBar.tsx
@@ -6,6 +6,9 @@ import {
   IconButton,
   Tooltip,
   useDisclosure,
+  Button,
+  Text,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import {
   faClose,
@@ -19,6 +22,9 @@ import { EDITOR_LANGUAGES, EDITOR_THEMES } from "../data/Editor";
 import ToolbarSettings from "./modals/ToolbarSettings";
 import { EditorSettingsContext } from "../contexts/editorSettingsContext";
 import FAIcon from "./FAIcon";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 interface ToolBarProps {
   isLoading?: boolean;
@@ -39,25 +45,36 @@ export default function ToolBar({
   const { isOpen, onOpen, onClose } = useDisclosure();
   const color = useColorModeValue("black", "white");
 
+  const router = useRouter();
+  const isPlayground = router.pathname === "/playground";
+
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+
   return (
     <>
       <HStack py={2} w={"full"} justifyContent="space-between">
         <Box>
-          <Select
-            color="white"
-            maxW={40}
-            value={lang}
-            onChange={(e) => {
-              updateSettings({ lang: e.target.value as Lang });
-            }}
-            fontWeight="extrabold"
-          >
-            {EDITOR_LANGUAGES.map((lang) => (
-              <option key={lang.value} style={{ color }} value={lang.value}>
-                {lang.label}
-              </option>
-            ))}
-          </Select>
+          {!isMobile && isPlayground ? (
+            <ToggleButtons lang={lang} langs={EDITOR_LANGUAGES} />
+          ) : (
+            <Select
+              color="white"
+              maxW={40}
+              value={lang}
+              onChange={(e) => {
+                if (!isPlayground)
+                  updateSettings({ lang: e.target.value as Lang });
+                else router.push(`?lang=${e.target.value}`);
+              }}
+              fontWeight="extrabold"
+            >
+              {EDITOR_LANGUAGES.map((lang) => (
+                <option key={lang.value} style={{ color }} value={lang.value}>
+                  {lang.label}
+                </option>
+              ))}
+            </Select>
+          )}
         </Box>
         <HStack>
           {onCodeReset && (
@@ -115,3 +132,57 @@ export default function ToolBar({
     </>
   );
 }
+
+const ToggleButtons = ({
+  lang,
+  langs,
+}: {
+  lang: Lang;
+  langs: { label: string; value: string; iconPath: string }[];
+}) => {
+  const router = useRouter();
+  const { lang: queryLang } = router.query as { lang: Lang };
+  const tabIndicatorStyles = {
+    py: { left: 10, width: 100 },
+    js: { left: 120, width: 120 },
+    cpp: { left: 250, width: 80 },
+    java: { left: 340, width: 80 },
+  };
+  return (
+    <HStack
+      pos={"relative"}
+      bg={"dark"}
+      px={2}
+      border={"1px solid #333"}
+      borderRadius={"8px"}
+    >
+      {langs.map((lang) => (
+        <Tooltip label={lang.label} key={lang.value}>
+          <Link href={"?lang=" + lang.value}>
+            <Button aria-label={lang.label} bg={"dark"} _hover={{ bg: "none" }}>
+              <HStack zIndex={2}>
+                <Image
+                  src={lang.iconPath}
+                  alt={lang.label}
+                  width={20}
+                  height={20}
+                />
+                <Text>{lang.label}</Text>
+              </HStack>
+            </Button>
+          </Link>
+        </Tooltip>
+      ))}
+      <Box
+        pos={"absolute"}
+        transition={"all 0.1s ease-out"}
+        left={tabIndicatorStyles[queryLang || "py"].left + "px"}
+        top={1}
+        w={tabIndicatorStyles[queryLang || "py"].width + "px"}
+        h={"calc(100% - 8px)"}
+        borderRadius={"8px"}
+        bg={"purple.700"}
+      ></Box>
+    </HStack>
+  );
+};
