7f0d7272795ae6def4fd471ae040eb9401c6b868
naresh-khatri <naresh.khatri2345@gmail.com>
2024-06-14 15:02:34 +0530

    feat: :sparkles: store all duck gifs

---
+++ constants/animated-ducks.ts
@@ -0,0 +1,43 @@
+const animatedDucks = {
+  greet: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/4foo_lx4UUc_jv.gif",
+  },
+  surprised: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/185d4d70936397.5bb439fbd97fc_fCcWMr1bG.gif",
+  },
+  motivate: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/motivate_Wb4QqXWs_.gif",
+  },
+  depressed: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/614ab270936397.5bb439fbdab5d_BzKOTRnS-.gif",
+  },
+  scared: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/0959c570936397.5bb439fbde3d2_lGsUwk-M9.gif",
+  },
+  sayNo: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/63d98670936397.5bb439fbdfabb_jxOOxoUWlm.gif",
+  },
+  slap: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/b9eb2b42094811.57c019c4dd143_D0CjrE5ta.gif",
+  },
+  impressive: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/impressive_JlIzccNNrR.gif",
+  },
+  shocked: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/2d037c70936397.5bb439fbda19f_leAvQOZbz.gif",
+  },
+  bamboozled: {
+    image:
+      "https://ik.imagekit.io/couponluxury/coding_ducks/gifs/sticker_21_WTFwUxilg.gif",
+  },
+};
+export default animatedDucks;
