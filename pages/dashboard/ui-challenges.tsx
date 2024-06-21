17b6e9b53a12058389b72c039461fa427ce4462d
naresh-khatri <naresh.khatri2345@gmail.com>
2024-06-21 19:26:05 +0530

    stuff

--- pages/dashboard/ui-challenges.tsx
+++ pages/dashboard/ui-challenges.tsx
@@ -1,6 +1,13 @@
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
   Box,
   Button,
+  Checkbox,
   Container,
   Flex,
   FormControl,
@@ -30,21 +37,24 @@ import {
   Tr,
   useDisclosure,
   useToast,
+  VStack,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import AdminLayout from "../../layout/AdminLayout";
 
 import {
+  useAChallengeAttemptsData,
   useChallengesData,
-  useRemoveChallenge,
-  useUpdateChallenge,
+  useARecalcScores,
+  useARemoveChallenge,
+  useAUpdateChallenge,
 } from "hooks/useChallengesData";
 import Image from "next/image";
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 import FAIcon from "components/FAIcon";
-import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faStar, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { IUIChallenge, UIChallengeDifficulty } from "types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CHALLENGE_DIFFICULTIES } from "constants/index";
 import { useRouter } from "next/router";
 import axiosInstance from "lib/axios";
@@ -52,6 +62,8 @@ import dynamic from "next/dynamic";
 import EditorsWithPreview from "components/editors/EditorsWithPreview";
 
 import "react-quill/dist/quill.snow.css";
+import { getTimeAgo } from "lib/formatDate";
+import UserAvatar from "components/utils/UserAvatar";
 const QuillNoSSRWrapper = dynamic(() => import("react-quill"), {
   ssr: false,
   loading: () => <p>Loading ...</p>,
@@ -100,23 +112,36 @@ function ChallengesPage() {
   } = useDisclosure();
 
   const [selectedChallenge, setSelectedChallenge] = useState<IUIChallenge>();
+  const [showArchive, setShowArchive] = useState(false);
+  const filteredChallenges = challengesData?.filter(
+    (challenge) => challenge.isPublic || showArchive
+  );
 
   if (challengesDataIsLoading || !challengesData) return <div>Loading...</div>;
 
   return (
     <AdminLayout>
-      <Container maxW="container.xl" overflow={"auto"}>
-        <HStack m={10}>
-          <Link href="/dashboard/add-ui-challenge">
-            <Button bg="green.400">Add Challenge</Button>
-          </Link>
-          <Button bg="green.400" onClick={() => refetchChallengeData()}>
-            Refresh
-          </Button>
+      <Flex direction={"column"} w={"full"} overflow={"auto"}>
+        <HStack p={10} w={"100%"} justifyContent={"space-between"}>
+          <HStack>
+            <Link href="/dashboard/add-ui-challenge">
+              <Button bg="green.400">Add Challenge</Button>
+            </Link>
+            <Button bg="green.400" onClick={() => refetchChallengeData()}>
+              Refresh
+            </Button>
+          </HStack>
+          <HStack>
+            <Checkbox
+              checked={showArchive}
+              onChange={(e) => setShowArchive(e.target.checked)}
+            >
+              Show Archive
+            </Checkbox>
+          </HStack>
         </HStack>
-
-        <TableContainer>
-          <Table variant="">
+        <TableContainer overflowY={"scroll"}>
+          <Table>
             <Thead>
               <Tr>
                 <Th isNumeric>ID</Th>
@@ -127,7 +152,7 @@ function ChallengesPage() {
               </Tr>
             </Thead>
             <Tbody>
-              {challengesData?.map((challenge) => (
+              {filteredChallenges?.map((challenge) => (
                 <Tr key={challenge.id}>
                   <Td isNumeric>{challenge.id}</Td>
                   <Td>
@@ -146,12 +171,20 @@ function ChallengesPage() {
                         height={300}
                         style={{ height: "150px", width: "auto" }}
                       />
+                      <Image
+                        src={challenge.ogImage}
+                        alt="preview"
+                        width={300}
+                        height={300}
+                        style={{ height: "150px", width: "auto" }}
+                      />
                     </HStack>
                   </Td>
                   <Td>{challenge.title}</Td>
                   <Td>{challenge.isPublic ? <CheckIcon /> : <CloseIcon />}</Td>
                   <Td>
                     <HStack>
+                      <ViewScoresModal challengeId={challenge.id} />
                       <IconButton
                         aria-label="Edit Exam"
                         icon={<FAIcon icon={faEdit} />}
@@ -206,12 +239,128 @@ function ChallengesPage() {
             refetch={refetchChallengeData}
           />
         )}
-      </Container>
+      </Flex>
     </AdminLayout>
   );
 }
 
 export default ChallengesPage;
+const ViewScoresModal = ({ challengeId }: { challengeId: number }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const finalRef = useRef(null);
+  const { data: attempts, isLoading: attemptsIsLoading } =
+    useAChallengeAttemptsData(challengeId);
+  const { mutate: recalculate, isLoading: isRecalculating } =
+    useARecalcScores();
+    const toast = useToast()
+  const handleOnRecalculateClick = (attemptId: number) => {
+    recalculate({ challengeId, attemptId }, {
+      onSuccess: (data) => {
+        toast({
+          title: "Recalculated",
+          description: "New score is" +data[0].score,
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        })
+      }
+    });
+  };
+  return (
+    <>
+      <IconButton
+        aria-label="view scores"
+        icon={<FAIcon icon={faStar} />}
+        onClick={onOpen}
+      />
+      <Modal
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        size={"6xl"}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>View Attempts</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <TableContainer>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th>id</Th>
+                    <Th>user</Th>
+                    <Th> time</Th>
+                    <Th>images</Th>
+                    <Th> score</Th>
+                    <Th> action</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {attempts?.map((attempt) => (
+                    <Tr key={attempt.id}>
+                      <Td>{attempt.id}</Td>
+                      <Td>
+                        <VStack>
+                          <UserAvatar
+                            alt="preview"
+                            src={attempt.user.photoURL}
+                            h={40}
+                            w={40}
+                          />
+                          <Text>{attempt.user.username}</Text>
+                        </VStack>
+                      </Td>
+                      <Td>{getTimeAgo(attempt.createdAt)}</Td>
+                      <Td>
+                        <HStack>
+                          <Image
+                            src={attempt.imgCode}
+                            alt="preview"
+                            width={300}
+                            height={300}
+                            style={{ height: "150px", width: "auto" }}
+                          />
+                        </HStack>
+                      </Td>
+                      <Td>{attempt.score}</Td>
+                      <Td>
+                        <HStack>
+                          <Button
+                            isLoading={isRecalculating}
+                            onClick={() => {
+                              handleOnRecalculateClick(attempt.id);
+                            }}
+                          >
+                            Recalculate
+                          </Button>
+                        </HStack>
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+                <Tfoot>
+                  <Tr>
+                    <Th>To convert</Th>
+                    <Th>into</Th>
+                    <Th isNumeric>multiply by</Th>
+                  </Tr>
+                </Tfoot>
+              </Table>
+            </TableContainer>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
 const EditModal = ({
   isOpen,
@@ -229,6 +378,7 @@ const EditModal = ({
   const [desc, setDesc] = useState(challengeData.description);
   const [isPublic, setIsPublic] = useState(challengeData.isPublic);
   const [diffLevel, setDiffLevel] = useState(challengeData.difficulty);
+  const [ogImageScale, setOgImageScale] = useState(challengeData.ogImageScale);
 
   const [content, setContent] = useState({
     head: challengeData.contentHEAD,
@@ -237,7 +387,8 @@ const EditModal = ({
     js: challengeData.contentJS,
   });
 
-  const { mutate, isLoading } = useUpdateChallenge();
+  const { mutate: updateChallenge, isLoading: challengeUpdating } =
+    useAUpdateChallenge();
 
   const toast = useToast();
 
@@ -256,7 +407,7 @@ const EditModal = ({
     }
   }, [title, slug, desc, diffLevel]);
 
-  const updateChallenge = async () => {
+  const handleUpdateClicked = async () => {
     const payload = {
       title,
       description: desc,
@@ -267,9 +418,10 @@ const EditModal = ({
       css: content.css,
       js: content.js,
       isPublic,
+      ogImageScale: ogImageScale,
     };
 
-    mutate(
+    updateChallenge(
       { challengeId: challengeData.id, payload },
       {
         onSuccess: () => {
@@ -372,8 +524,36 @@ const EditModal = ({
                   onChange={(e) => setIsPublic(e.target.checked)}
                 />
               </FormControl>
+              <FormControl>
+                <FormLabel
+                  htmlFor="og-img-scale"
+                  display={"flex"}
+                  gap={2}
+                  justifyContent={"space-between"}
+                  alignItems={"center"}
+                >
+                  OG Image Scale
+                </FormLabel>
+                <Select
+                  id="og-img-scale"
+                  value={ogImageScale}
+                  onChange={(e) =>
+                    // @ts-ignore
+                    setOgImageScale(+e.target.value)
+                  }
+                >
+                  <option value="0">Ignore</option>
+                  <option value="1">1x</option>
+                  <option value="2">2x</option>
+                  <option value="3">3x</option>
+                  <option value="4">4x</option>
+                </Select>
+              </FormControl>
             </HStack>
 
+            <HStack justifyContent={"end"}>
+              <RecalculateScoreAlert challengeId={challengeData.id} />
+            </HStack>
             <FormControl mt="2%">
               <FormLabel htmlFor="description" fontWeight={"normal"}>
                 Description
@@ -426,8 +606,8 @@ const EditModal = ({
             </Button>
             <Button
               colorScheme="purple"
-              onClick={updateChallenge}
-              isLoading={isLoading}
+              onClick={handleUpdateClicked}
+              isLoading={challengeUpdating}
             >
               update
             </Button>
@@ -437,6 +617,56 @@ const EditModal = ({
     </Modal>
   );
 };
+function RecalculateScoreAlert({ challengeId }: { challengeId: number }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef(null);
+  const { mutate: recalculateScores, isLoading: recalculatingScores } =
+    useARecalcScores();
+
+  return (
+    <>
+      <Button
+        colorScheme="red"
+        onClick={onOpen}
+        isLoading={recalculatingScores}
+      >
+        Recalculate Scores
+      </Button>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Recalculate Scores?
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can&apos;t undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={() => recalculateScores({ challengeId })}
+                ml={3}
+                isLoading={recalculatingScores}
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
 
 const DeleteModal = ({
   isOpen,
@@ -449,7 +679,7 @@ const DeleteModal = ({
   challengeData: IUIChallenge;
   refetch: () => void;
 }) => {
-  const { mutate, isLoading } = useRemoveChallenge();
+  const { mutate, isLoading } = useARemoveChallenge();
 
   const toast = useToast();
 
