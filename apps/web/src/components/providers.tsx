f24aa53831bcb831ddf4de582be6276422b5f9c9
naresh <naresh.khatri2345@gmail.com>
2026-04-05 22:09:35 +0530

    chore(web): add reactflow, simple-icons and mount sonner toaster

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -2,6 +2,7 @@
 
 import { TRPCReactProvider } from "~/trpc/react";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "./ui/sonner";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,6 +13,7 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
       disableTransitionOnChange
     >
       <TRPCReactProvider>{children}</TRPCReactProvider>
+      <Toaster richColors />
     </ThemeProvider>
   );
 };
